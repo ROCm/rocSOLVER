8237380c4370255cee0cb9c2536854ead9a61a5f
Cordell Bloor <cordell.bloor@amd.com>
2020-07-08 23:09:25 -0600

    Print rocsolver and rocblas version in tests
    It's now both the first line and the last line, matching rocBLAS's
    behaviour. The footer is particularly handy, as it ensures the version
    is in the tail of the log next to any failed test results.

--- rocsolver/clients/gtest/rocsolver_gtest_main.cpp
+++ rocsolver/clients/gtest/rocsolver_gtest_main.cpp
@@ -3,30 +3,45 @@
  * ************************************************************************ */
 
 #include "clientcommon.hpp"
+#include "rocblas-version.h"
+#include "rocsolver-version.h"
 #include <gtest/gtest.h>
 #include <stdexcept>
 
-/* =====================================================================
-      Main function:
-=================================================================== */
+#define STRINGIFY(s) STRINGIFY_HELPER(s)
+#define STRINGIFY_HELPER(s) #s
 
-int main(int argc, char **argv) {
+static void print_version_info()
+{
+    rocblas_cout << "rocSOLVER version "
+        STRINGIFY(ROCSOLVER_VERSION_MAJOR) "."
+        STRINGIFY(ROCSOLVER_VERSION_MINOR) "."
+        STRINGIFY(ROCSOLVER_VERSION_PATCH) "."
+        STRINGIFY(ROCSOLVER_VERSION_TWEAK)
+        " (with rocBLAS "
+        STRINGIFY(ROCBLAS_VERSION_MAJOR) "."
+        STRINGIFY(ROCBLAS_VERSION_MINOR) "."
+        STRINGIFY(ROCBLAS_VERSION_PATCH) "."
+        STRINGIFY(ROCBLAS_VERSION_TWEAK) ")"
+        << std::endl;
+}
 
-  // Device Query
+int main(int argc, char **argv)
+{
+    print_version_info();
 
-  int device_id = 0;
-
-  int device_count = query_device_property();
-
-  if (device_count <= device_id) {
-    //printf("Error: invalid device ID. There may not be such device ID. Will exit \n");
-    rocblas_cout << "Error: invalid device ID. There may not be such device ID. Will exit \n";
-    return -1;
-  } else {
+    // Device Query
+    int device_id = 0;
+    int device_count = query_device_property();
+    if (device_count <= device_id) {
+        rocblas_cerr << "Error: invalid device ID. There may not be such device ID." << std::endl;
+        return -1;
+    }
     set_device(device_id);
-  }
 
-  ::testing::InitGoogleTest(&argc, argv);
+    ::testing::InitGoogleTest(&argc, argv);
 
-  return RUN_ALL_TESTS();
+    int status = RUN_ALL_TESTS();
+    print_version_info(); // redundant, but convenient when tests fail
+    return status;
 }
