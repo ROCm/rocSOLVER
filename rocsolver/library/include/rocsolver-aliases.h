290351b532025bdd32e59df56291f4eebca6f2dd
Cordell Bloor <cordell.bloor@amd.com>
2020-07-16 12:52:48 -0600

    Document deprecated types and functions

--- rocsolver/library/include/rocsolver-aliases.h
+++ rocsolver/library/include/rocsolver-aliases.h
@@ -19,42 +19,89 @@
 #include "rocsolver-export.h"
 
 //rocblas original types
+
+/*! \deprecated Use ::rocblas_int.
+*/
 typedef rocblas_int rocsolver_int
         __attribute__((deprecatred("use rocblas_int")));
+
+/*! \deprecated Use ::rocblas_stride.
+*/
 typedef rocblas_stride rocsolver_stride
         __attribute__((deprecatred("use rocblas_stride")));
+
+/*! \deprecated Use ::rocblas_float_complex.
+*/
 typedef rocblas_float_complex rocsolver_float_complex
         __attribute__((deprecatred("use rocblas_float_complex")));
+
+/*! \deprecated Use ::rocblas_double_complex.
+*/
 typedef rocblas_double_complex rocsolver_double_complex
         __attribute__((deprecatred("use rocblas_double_complex")));
+
+/*! \deprecated Use ::rocblas_half.
+*/
 typedef rocblas_half rocsolver_half
         __attribute__((deprecatred("use rocblas_half")));
+
+/*! \deprecated Use ::rocblas_handle.
+*/
 typedef rocblas_handle rocsolver_handle
         __attribute__((deprecatred("use rocblas_handle")));
+
+/*! \deprecated Use ::rocblas_operation.
+*/
 typedef rocblas_operation rocsolver_operation
         __attribute__((deprecatred("use rocblas_operation")));
+
+/*! \deprecated Use ::rocblas_fill.
+*/
 typedef rocblas_fill rocsolver_fill
         __attribute__((deprecatred("use rocblas_fill")));
+
+/*! \deprecated Use ::rocblas_diagonal.
+*/
 typedef rocblas_diagonal rocsolver_diagonal
         __attribute__((deprecatred("use rocblas_diagonal")));
+
+/*! \deprecated Use ::rocblas_stide.
+*/
 typedef rocblas_side rocsolver_side;
         __attribute__((deprecatred("use rocblas_side")));
+
+/*! \deprecated Use ::rocblas_status.
+*/
 typedef rocblas_status rocsolver_status;
         __attribute__((deprecatred("use rocblas_status")));
+
+/*! \deprecated Use ::rocblas_layer_mode.
+*/
 typedef rocblas_layer_mode rocsolver_layer_mode;
         __attribute__((deprecatred("use rocblas_layer_mode")));
 
+
 //extras types only used in rocsolver
+
+/*! \deprecated Use ::rocblas_direction.
+*/
 typedef rocblas_direct rocsolver_direction
         __attribute__((deprecatred("use rocblas_direction")));
+
+/*! \deprecated Use ::rocblas_storev.
+*/
 typedef rocblas_storev rocsolver_storev
         __attribute__((deprecatred("use rocblas_storev")));
+
 
 //auxiliaries
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+/*! \brief Creates a handle and sets the pointer mode to ::rocblas_pointer_mode_device.
+    \deprecated Use ::rocblas_create_handle.
+*/
 ROCSOLVER_EXPORT __inline rocsolver_status
 rocsolver_create_handle(rocsolver_handle *handle)
         __attribute__((deprecatred("use rocblas_create_handle")))
@@ -66,6 +113,8 @@ rocsolver_create_handle(rocsolver_handle *handle)
   return rocblas_set_pointer_mode(*handle, rocblas_pointer_mode_device);
 }
 
+/*! \deprecated Use ::rocblas_destroy_handle.
+*/
 ROCSOLVER_EXPORT __inline rocsolver_status
 rocsolver_destroy_handle(rocsolver_handle handle)
         __attribute__((deprecatred("use rocblas_destroy_handle")))
@@ -80,6 +129,8 @@ rocsolver_destroy_handle(rocsolver_handle handle)
 //  return rocblas_add_stream(handle, stream);
 //}
 
+/*! \deprecated Use ::rocblas_set_stream.
+*/
 ROCSOLVER_EXPORT __inline rocsolver_status
 rocsolver_set_stream(rocsolver_handle handle, hipStream_t stream)
         __attribute__((deprecatred("use rocblas_set_stream")))
@@ -87,6 +138,8 @@ rocsolver_set_stream(rocsolver_handle handle, hipStream_t stream)
   return rocblas_set_stream(handle, stream);
 }
 
+/*! \deprecated Use ::rocblas_get_stream.
+*/
 ROCSOLVER_EXPORT __inline rocsolver_status
 rocsolver_get_stream(rocsolver_handle handle, hipStream_t *stream)
         __attribute__((deprecatred("use rocblas_get_stream")))
@@ -94,6 +147,8 @@ rocsolver_get_stream(rocsolver_handle handle, hipStream_t *stream)
   return rocblas_get_stream(handle, stream);
 }
 
+/*! \deprecated Use ::rocblas_set_vector.
+*/
 ROCSOLVER_EXPORT __inline rocsolver_status
 rocsolver_set_vector(rocsolver_int n, rocsolver_int elem_size, const void *x,
                      rocsolver_int incx, void *y, rocsolver_int incy)
@@ -102,6 +157,8 @@ rocsolver_set_vector(rocsolver_int n, rocsolver_int elem_size, const void *x,
   return rocblas_set_vector(n, elem_size, x, incx, y, incy);
 }
 
+/*! \deprecated Use ::rocblas_get_vector.
+*/
 ROCSOLVER_EXPORT __inline rocsolver_status
 rocsolver_get_vector(rocsolver_int n, rocsolver_int elem_size, const void *x,
                      rocsolver_int incx, void *y, rocsolver_int incy)
@@ -110,6 +167,8 @@ rocsolver_get_vector(rocsolver_int n, rocsolver_int elem_size, const void *x,
   return rocblas_get_vector(n, elem_size, x, incx, y, incy);
 }
 
+/*! \deprecated Use ::rocblas_set_matrix.
+*/
 ROCSOLVER_EXPORT __inline rocsolver_status
 rocsolver_set_matrix(rocsolver_int rows, rocsolver_int cols,
                      rocsolver_int elem_size, const void *a, rocsolver_int lda,
@@ -119,6 +178,8 @@ rocsolver_set_matrix(rocsolver_int rows, rocsolver_int cols,
   return rocblas_set_matrix(rows, cols, elem_size, a, lda, b, ldb);
 }
 
+/*! \deprecated Use ::rocblas_get_matrix.
+*/
 ROCSOLVER_EXPORT __inline rocsolver_status
 rocsolver_get_matrix(rocsolver_int rows, rocsolver_int cols,
                      rocsolver_int elem_size, const void *a, rocsolver_int lda,
