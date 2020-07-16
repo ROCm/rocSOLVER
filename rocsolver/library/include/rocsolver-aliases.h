09270f5d3ac91482ace9600bf3abf6974de8985f
Cordell Bloor <cordell.bloor@amd.com>
2020-07-16 14:24:25 -0600

    Switch rocblas Doxygen links to styled text
    We would need a rocBLAS Doxygen TAGFILE to link to the actual types,
    and I'm not sure how to set that up yet.

--- rocsolver/library/include/rocsolver-aliases.h
+++ rocsolver/library/include/rocsolver-aliases.h
@@ -20,62 +20,62 @@
 
 //rocblas original types
 
-/*! \deprecated Use ::rocblas_int.
+/*! \deprecated Use \c rocblas_int.
 */
 typedef rocblas_int rocsolver_int
         __attribute__((deprecated("use rocblas_int")));
 
-/*! \deprecated Use ::rocblas_stride.
+/*! \deprecated Use \c rocblas_stride.
 */
 typedef rocblas_stride rocsolver_stride
         __attribute__((deprecated("use rocblas_stride")));
 
-/*! \deprecated Use ::rocblas_float_complex.
+/*! \deprecated Use \c rocblas_float_complex.
 */
 typedef rocblas_float_complex rocsolver_float_complex
         __attribute__((deprecated("use rocblas_float_complex")));
 
-/*! \deprecated Use ::rocblas_double_complex.
+/*! \deprecated Use \c rocblas_double_complex.
 */
 typedef rocblas_double_complex rocsolver_double_complex
         __attribute__((deprecated("use rocblas_double_complex")));
 
-/*! \deprecated Use ::rocblas_half.
+/*! \deprecated Use \c rocblas_half.
 */
 typedef rocblas_half rocsolver_half
         __attribute__((deprecated("use rocblas_half")));
 
-/*! \deprecated Use ::rocblas_handle.
+/*! \deprecated Use \c rocblas_handle.
 */
 typedef rocblas_handle rocsolver_handle
         __attribute__((deprecated("use rocblas_handle")));
 
-/*! \deprecated Use ::rocblas_operation.
+/*! \deprecated Use \c rocblas_operation.
 */
 typedef rocblas_operation rocsolver_operation
         __attribute__((deprecated("use rocblas_operation")));
 
-/*! \deprecated Use ::rocblas_fill.
+/*! \deprecated Use \c rocblas_fill.
 */
 typedef rocblas_fill rocsolver_fill
         __attribute__((deprecated("use rocblas_fill")));
 
-/*! \deprecated Use ::rocblas_diagonal.
+/*! \deprecated Use \c rocblas_diagonal.
 */
 typedef rocblas_diagonal rocsolver_diagonal
         __attribute__((deprecated("use rocblas_diagonal")));
 
-/*! \deprecated Use ::rocblas_stide.
+/*! \deprecated Use \c rocblas_stide.
 */
 typedef rocblas_side rocsolver_side
         __attribute__((deprecated("use rocblas_side")));
 
-/*! \deprecated Use ::rocblas_status.
+/*! \deprecated Use \c rocblas_status.
 */
 typedef rocblas_status rocsolver_status
         __attribute__((deprecated("use rocblas_status")));
 
-/*! \deprecated Use ::rocblas_layer_mode.
+/*! \deprecated Use \c rocblas_layer_mode.
 */
 typedef rocblas_layer_mode rocsolver_layer_mode
         __attribute__((deprecated("use rocblas_layer_mode")));
@@ -83,12 +83,12 @@ typedef rocblas_layer_mode rocsolver_layer_mode
 
 //extras types only used in rocsolver
 
-/*! \deprecated Use ::rocblas_direction.
+/*! \deprecated Use \c rocblas_direction.
 */
 typedef rocblas_direct rocsolver_direction
         __attribute__((deprecated("use rocblas_direction")));
 
-/*! \deprecated Use ::rocblas_storev.
+/*! \deprecated Use \c rocblas_storev.
 */
 typedef rocblas_storev rocsolver_storev
         __attribute__((deprecated("use rocblas_storev")));
@@ -103,8 +103,8 @@ extern "C" {
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
 
-/*! \brief Creates a handle and sets the pointer mode to ::rocblas_pointer_mode_device.
-    \deprecated Use ::rocblas_create_handle.
+/*! \brief Creates a handle and sets the pointer mode to \c rocblas_pointer_mode_device.
+    \deprecated Use \c rocblas_create_handle.
 */
 ROCSOLVER_EXPORT __inline rocsolver_status
 rocsolver_create_handle(rocsolver_handle *handle)
@@ -118,7 +118,7 @@ rocsolver_create_handle(rocsolver_handle *handle) {
   return rocblas_set_pointer_mode(*handle, rocblas_pointer_mode_device);
 }
 
-/*! \deprecated Use ::rocblas_destroy_handle.
+/*! \deprecated Use \c rocblas_destroy_handle.
 */
 ROCSOLVER_EXPORT rocsolver_status
 rocsolver_destroy_handle(rocsolver_handle handle)
@@ -135,7 +135,7 @@ rocsolver_destroy_handle(rocsolver_handle handle) {
 //  return rocblas_add_stream(handle, stream);
 //}
 
-/*! \deprecated Use ::rocblas_set_stream.
+/*! \deprecated Use \c rocblas_set_stream.
 */
 ROCSOLVER_EXPORT rocsolver_status
 rocsolver_set_stream(rocsolver_handle handle, hipStream_t stream)
@@ -145,7 +145,7 @@ rocsolver_set_stream(rocsolver_handle handle, hipStream_t stream) {
   return rocblas_set_stream(handle, stream);
 }
 
-/*! \deprecated Use ::rocblas_get_stream.
+/*! \deprecated Use \c rocblas_get_stream.
 */
 ROCSOLVER_EXPORT rocsolver_status
 rocsolver_get_stream(rocsolver_handle handle, hipStream_t *stream)
@@ -155,7 +155,7 @@ rocsolver_get_stream(rocsolver_handle handle, hipStream_t *stream) {
   return rocblas_get_stream(handle, stream);
 }
 
-/*! \deprecated Use ::rocblas_set_vector.
+/*! \deprecated Use \c rocblas_set_vector.
 */
 ROCSOLVER_EXPORT rocsolver_status
 rocsolver_set_vector(rocsolver_int n, rocsolver_int elem_size, const void *x,
@@ -167,7 +167,7 @@ rocsolver_set_vector(rocsolver_int n, rocsolver_int elem_size, const void *x,
   return rocblas_set_vector(n, elem_size, x, incx, y, incy);
 }
 
-/*! \deprecated Use ::rocblas_get_vector.
+/*! \deprecated Use \c rocblas_get_vector.
 */
 ROCSOLVER_EXPORT rocsolver_status
 rocsolver_get_vector(rocsolver_int n, rocsolver_int elem_size, const void *x,
@@ -179,7 +179,7 @@ rocsolver_get_vector(rocsolver_int n, rocsolver_int elem_size, const void *x,
   return rocblas_get_vector(n, elem_size, x, incx, y, incy);
 }
 
-/*! \deprecated Use ::rocblas_set_matrix.
+/*! \deprecated Use \c rocblas_set_matrix.
 */
 ROCSOLVER_EXPORT rocsolver_status
 rocsolver_set_matrix(rocsolver_int rows, rocsolver_int cols,
@@ -193,7 +193,7 @@ rocsolver_set_matrix(rocsolver_int rows, rocsolver_int cols,
   return rocblas_set_matrix(rows, cols, elem_size, a, lda, b, ldb);
 }
 
-/*! \deprecated Use ::rocblas_get_matrix.
+/*! \deprecated Use \c rocblas_get_matrix.
 */
 ROCSOLVER_EXPORT rocsolver_status
 rocsolver_get_matrix(rocsolver_int rows, rocsolver_int cols,
