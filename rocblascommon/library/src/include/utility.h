25a9e6a87db4c515f3e0b4430f4519378f3cb405
Troy Alderson <58866654+tfalders@users.noreply.github.com>
2021-01-15 15:42:36 -0700

    Fixed hang in client code caused by rocsolver_ostream

--- rocblascommon/library/src/include/utility.h
+++ rocblascommon/library/src/include/utility.h
@@ -1,11 +1,12 @@
 /* ************************************************************************
- * Copyright (c) 2016-2020 Advanced Micro Devices, Inc.
+ * Copyright (c) 2016-2021 Advanced Micro Devices, Inc.
  * ************************************************************************ */
 
 #pragma once
 
 #include "definitions.h"
 #include "rocblas.h"
+#include "rocsolver.h"
 #include <cmath>
 #include <complex>
 #include <exception>
@@ -212,6 +213,59 @@ constexpr char rocblas_diag_letter(rocblas_diagonal diag)
     {
     case rocblas_diagonal_non_unit: return 'N';
     case rocblas_diagonal_unit:     return 'U';
+    }
+    return ' ';
+}
+
+constexpr char rocblas_direct_letter(rocblas_direct value)
+{
+    switch(value)
+    {
+    case rocblas_forward_direction: return 'F';
+    case rocblas_backward_direction: return 'B';
+    }
+    return ' ';
+}
+
+constexpr char rocblas_storev_letter(rocblas_storev value)
+{
+    switch(value)
+    {
+    case rocblas_column_wise: return 'C';
+    case rocblas_row_wise: return 'R';
+    }
+    return ' ';
+}
+
+constexpr char rocblas_workmode_letter(rocblas_workmode value)
+{
+    switch(value)
+    {
+    case rocblas_outofplace: return 'O';
+    case rocblas_inplace: return 'I';
+    }
+    return ' ';
+}
+
+constexpr char rocblas_svect_letter(rocblas_svect value)
+{
+    switch(value)
+    {
+    case rocblas_svect_all: return 'A';
+    case rocblas_svect_singular: return 'S';
+    case rocblas_svect_overwrite: return 'O';
+    case rocblas_svect_none: return 'N';
+    }
+    return ' ';
+}
+
+constexpr char rocblas_evect_letter(rocblas_evect value)
+{
+    switch(value)
+    {
+    case rocblas_evect_original: return 'V';
+    case rocblas_evect_tridiagonal: return 'I';
+    case rocblas_evect_none: return 'N';
     }
     return ' ';
 }
