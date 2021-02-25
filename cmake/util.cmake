68197d61bf6f06ff5b2465a9eb27bfe11a4259f9
Cory Bloor <Cordell.Bloor@amd.com>
2021-02-25 15:48:56 -0700

    Add rocblas dependency to CMake and OS packages - build system - 4/5 (#218)
    * Print rocblas location during CMake configure
    
    * Specify rocsolver dependency on rocblas
    
    With the CMake interface requirements set properly, there's a lot of
    redundant CMake code that can be deleted.
    
    * Remove BUILD_WITH_TENSILE define
    
    BUILD_WITH_TENSILE is an internal rocblas macro. It does not appear in the rocSOLVER
    repository, nor in the public or internal rocBLAS headers.
    
    * Add rocblas to installed CMake targets
    
    Adding rocblas to rocm_export_targets ensures that
    find_package(rocsolver) will internally do find_package(rocblas).
    
    * Add CHANGELOG entry
    
    * Allow rocblas package patch version to vary
    
    We set the debian_revision version / tweak version as the git hash,
    so exact comparisons will break for development packages.
    
    [ROCm/rocSOLVER commit: 5004d4e8d2c0ec5c392f0256d10b8a1d59c2121b]

--- cmake/util.cmake
+++ cmake/util.cmake
@@ -59,3 +59,20 @@ function( prepend_path prefix source_list_of_files return_list_of_files )
   set( ${return_list_of_files} ${new_list} PARENT_SCOPE )
 endfunction( )
 
+# ########################################################################
+# Search through the location properties to find one that is set
+# ########################################################################
+function( get_imported_target_location result_variable imported_target )
+  set( properties_to_search
+    IMPORTED_LOCATION
+    IMPORTED_LOCATION_RELEASE
+    IMPORTED_LOCATION_DEBUG)
+  foreach( property ${properties_to_search} )
+    get_target_property( location "${imported_target}" "${property}" )
+    if( location )
+      set( "${result_variable}" "${location}" PARENT_SCOPE )
+      return()
+    endif( )
+  endforeach( )
+  set( "${result_variable}" "${result_variable}-NOTFOUND" PARENT_SCOPE )
+endfunction( )
