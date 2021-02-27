f554129bbd08fb8ad522a58e7af2c36159feb5e2
Juan Zuniga-Anaya <50754207+jzuniga-amd@users.noreply.github.com>
2021-02-26 17:35:00 -0700

    prepare 3.12 release for ROCm4.2 (#225)
    [ROCm/rocSOLVER commit: 124b3bc2560aae50ed1fac115160e00577f146ce]

--- bump_rocsolver_version.sh
+++ bump_rocsolver_version.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 
 # This script needs to be edited to bump rocsolver version for new release.
-# - run this script in develop before merging developi/staging into master
+# - run this script in develop before merging develop/staging into master
 
-OLD_ROCSOLVER_VERSION="3.10.0"  #Released in October/November 2020
-NEW_ROCSOLVER_VERSION="3.11.0"  #To release in February 2021
+OLD_ROCSOLVER_VERSION="3.11.0"  #Released in February/March 2021
+NEW_ROCSOLVER_VERSION="3.12.0"  #To release in April/May 2021
 sed -i "s/${OLD_ROCSOLVER_VERSION}/${NEW_ROCSOLVER_VERSION}/g" CMakeLists.txt
 
 # for documentation
+OLD_ROCSOLVER_DOCS_VERSION="3.11"
+NEW_ROCSOLVER_DOCS_VERSION="3.12"
+sed -i "s/${OLD_ROCSOLVER_DOCS_VERSION}/${NEW_ROCSOLVER_DOCS_VERSION}/g" docs/source/conf.py
 
-OLD_ROCSOLVER_VERSION="3.10"
-NEW_ROCSOLVER_VERSION="3.11"
-sed -i "s/${OLD_ROCSOLVER_VERSION}/${NEW_ROCSOLVER_VERSION}/g" docs/source/conf.py
+# for rocBLAS package requirements
+OLD_ROCBLAS_VERSION_DOWN="2.37"
+NEW_ROCBLAS_VERSION_DOWN="2.38"
+OLD_ROCBLAS_VERSION_UP="2.38"
+NEW_ROCBLAS_VERSION_UP="2.39"
+sed -i "s/${OLD_ROCBLAS_VERSION_UP}/${NEW_ROCBLAS_VERSION_UP}/g" library/CMakeLists.txt
+sed -i "s/${OLD_ROCBLAS_VERSION_DOWN}/${NEW_ROCBLAS_VERSION_DOWN}/g" library/CMakeLists.txt
+
