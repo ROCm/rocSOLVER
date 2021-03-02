c76b7983692d29f12307728c27c109325930e3cf
Troy Alderson <58866654+tfalders@users.noreply.github.com>
2021-03-02 14:27:24 -0700

    Fix rocSOLVER version bump

--- bump_rocsolver_version.sh
+++ bump_rocsolver_version.sh
@@ -3,13 +3,13 @@
 # This script needs to be edited to bump rocsolver version for new release.
 # - run this script in develop before merging develop/staging into master
 
-OLD_ROCSOLVER_VERSION="3.11.0"  #Released in February/March 2021
-NEW_ROCSOLVER_VERSION="3.12.0"  #To release in April/May 2021
+OLD_ROCSOLVER_VERSION="3.12.0"  #Released in February/March 2021
+NEW_ROCSOLVER_VERSION="3.13.0"  #To release in April/May 2021
 sed -i "s/${OLD_ROCSOLVER_VERSION}/${NEW_ROCSOLVER_VERSION}/g" CMakeLists.txt
 
 # for documentation
-OLD_ROCSOLVER_DOCS_VERSION="3.11"
-NEW_ROCSOLVER_DOCS_VERSION="3.12"
+OLD_ROCSOLVER_DOCS_VERSION="3.12"
+NEW_ROCSOLVER_DOCS_VERSION="3.13"
 sed -i "s/${OLD_ROCSOLVER_DOCS_VERSION}/${NEW_ROCSOLVER_DOCS_VERSION}/g" docs/source/conf.py
 
 # for rocBLAS package requirements
