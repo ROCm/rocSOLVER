d57dfb28464a83c808f7a47e91d8a5b2a430ad9f
Cory Bloor <Cordell.Bloor@amd.com>
2021-03-26 17:18:42 -0600

    Remove compiler version parsing (#231)
    * Delete compiler-detection paths in build scripts
    
    rocSOLVER is not built or tested with nvcc, which makes the existance
    of these incomplete checks somewhat misleading. Eliminating these
    code paths helps to simplify the build system.
    
    * Restore __HIP_HCC_COMPAT_MODE__=1 define
    
    I'm not sure if this define is really needed, but I wanted this pull
    request to be purely refactoring (i.e., no actual changes to the
    build flags that are ultimately passed to the compiler).
    
    * Drop incomplete CUDA support from build scripts
    
    The CUDA build paths never worked in rocSOLVER. The partial support
    that existed within the build scripts were just leftovers from the
    older projects that these build scripts were copied from.
    
    hipSOLVER is the API that supports both ROCm and CUDA.
    
    [ROCm/rocSOLVER commit: f0cbc734bdccc96f088287b7ebe16c2c3d6c469d]

--- cmake/util.cmake
+++ cmake/util.cmake
@@ -3,34 +3,6 @@
 # ########################################################################
 
 # ########################################################################
-# target_link_libraries() override
-# Wraps the normal cmake function to cope with hipcc/nvcc weirdness.
-# ########################################################################
-function( target_link_libraries target_name )
-  # hipcc takes care of finding hip library dependencies internally; remove
-  # explicit mentions of them so cmake doesn't complain on nvcc path
-  # FIXME: This removes this target's hip libraries, even if this particular
-  #        target is not built using a compiler that can find its dependencies
-  #        internally (e.g. Fortran programs).
-  if( CUDA_FOUND AND CMAKE_CXX_COMPILER MATCHES ".*/hipcc$|.*/nvcc$" )
-    foreach( link_library ${ARGN} )
-      if( (link_library MATCHES "^hip::") )
-        message( DEBUG "Removing ${link_library} from ${target_name} library list" )
-      else( )
-        if( TARGET ${link_library} )
-          list( APPEND new_list -Xlinker ${link_library} )
-        else( )
-          list( APPEND new_list ${link_library} )
-        endif( )
-      endif( )
-    endforeach( )
-    _target_link_libraries( ${target_name} ${new_list} )
-  else( )
-    _target_link_libraries( ${target_name} ${ARGN} )
-  endif( )
-endfunction( )
-
-# ########################################################################
 # A helper function to prefix a source list of files with a common path
 # into a new list (non-destructive)
 # ########################################################################
