47f7a19d7d87671ee2db8fd8f358dfbf40eb93d9
Cordell Bloor <Cordell.Bloor@amd.com>
2021-03-30 19:35:28 +0000

    Remove inaccurate comment
    ./install.sh for hip-clang was changed to prepend to the PATH in
    6602627f0196756da237fbeaa9d82b82704b6b3c, though we continued to
    append to the PATH for hcc. Since hcc support has ben removed, the
    comment is no longer accurate.

--- install.sh
+++ install.sh
@@ -405,9 +405,6 @@ esac
 export CXX="hipcc"
 export CC="clang"
 export FC="gfortran"
-
-# We append customary rocm path; if user provides custom rocm path in ${path}, our
-# hard-coded path has lesser priority
 export PATH="${rocm_path}/bin:${rocm_path}/hip/bin:${rocm_path}/llvm/bin:${PATH}"
 
 # #################################################
