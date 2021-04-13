3e1da6cf216bfb2a7cc20df242591e63b7182035
Cory Bloor <Cordell.Bloor@amd.com>
2021-04-12 23:10:20 -0600

    Remove unnecessary ROCSOLVER_COMMON namespace (#238)
    Symbols marked as hidden can be repeated in another executable, and
    both sets of symbols will be considered distinct from each other. This
    namespace is therefore unnecessary.

--- common/include/common_host_helpers.hpp
+++ common/include/common_host_helpers.hpp
@@ -6,8 +6,6 @@
 
 #include "rocsolver_ostream.hpp"
 
-namespace ROCSOLVER_COMMON_NAMESPACE
-{
 /*
  * ===========================================================================
  *    common location for functions that are used by both the rocSOLVER
@@ -220,7 +218,3 @@ void print_host_matrix(rocsolver_ostream& os,
     }
     os << std::endl;
 }
-
-}
-
-using namespace ROCSOLVER_COMMON_NAMESPACE;
