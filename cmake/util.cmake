be363db7190353cd36d3f2ad008abccb035de3ef
Cordell Bloor <Cordell.Bloor@amd.com>
2021-06-14 21:39:28 +0000

    Reformat CMake code
    This change reformats all rocsolver CMake code into a consistent coding
    style, with the exception of deps/* (which I hope can be replaced
    entirely in the near future).
    
    The rules followed were:
      - Indent with two spaces.
      - 100 character maximum line width.
      - Begin comments with a capital letter.
      - Do not include a space before or after function parentheses.
      - Any number of unrelated function arguments may go on the same line,
        but wrap when they would exceed the maximum line width.
      - Functions taking multi-value arguments are split across multiple
        lines under the following conditions:
        - A multi-valued argument is moved to new line and indented one
          level if there's more than one multi-valued argument being passed
          (e.g.  both a PRIVATE and a PUBLIC category).
        - A multi-valued arument's values are moved to a new line and
          indented if there's more than one value being passed for that
          argument.
      - Functions taking a list of items are treated like those taking
        multi-value arguments, i.e., if there are multiple values being
        passed, the values should each be on a seperate line.
      - Multi-value functions split across multiple lines are closed with
        a parenthesis at the same indentation level as the function name.
        (Unlike functions that were wrapped only due to length, which have
        the closing parenthesis immediately following the last argument.)

--- cmake/util.cmake
+++ cmake/util.cmake
@@ -1,50 +1,45 @@
 # ########################################################################
-# HELPER FUNCTIONS
+# Copyright (c) 2021 Advanced Micro Devices, Inc.
 # ########################################################################
 
-# ########################################################################
 # A helper function to prefix a source list of files with a common path
 # into a new list (non-destructive)
-# ########################################################################
-function( prepend_path prefix source_list_of_files return_list_of_files )
-  foreach( file ${${source_list_of_files}} )
-    if(IS_ABSOLUTE ${file} )
-      list( APPEND new_list ${file} )
-    else( )
-      list( APPEND new_list ${prefix}/${file} )
-    endif( )
-  endforeach( )
-  set( ${return_list_of_files} ${new_list} PARENT_SCOPE )
-endfunction( )
+function(prepend_path prefix source_list_of_files return_list_of_files)
+  foreach(file ${${source_list_of_files}})
+    if(IS_ABSOLUTE ${file})
+      list(APPEND new_list ${file})
+    else()
+      list(APPEND new_list ${prefix}/${file})
+    endif()
+  endforeach()
+  set(${return_list_of_files} ${new_list} PARENT_SCOPE)
+endfunction()
 
-# ########################################################################
 # Search through the location properties to find one that is set
-# ########################################################################
-function( get_imported_target_location result_variable imported_target )
-  set( properties_to_search
+function(get_imported_target_location result_variable imported_target)
+  set(properties_to_search
     IMPORTED_LOCATION
     IMPORTED_LOCATION_RELEASE
-    IMPORTED_LOCATION_DEBUG)
-  foreach( property ${properties_to_search} )
-    get_target_property( location "${imported_target}" "${property}" )
-    if( location )
-      set( "${result_variable}" "${location}" PARENT_SCOPE )
+    IMPORTED_LOCATION_DEBUG
+  )
+  foreach(property ${properties_to_search})
+    get_target_property(location "${imported_target}" "${property}")
+    if(location)
+      set("${result_variable}" "${location}" PARENT_SCOPE)
       return()
-    endif( )
-  endforeach( )
-  set( "${result_variable}" "${result_variable}-NOTFOUND" PARENT_SCOPE )
-endfunction( )
+    endif()
+  endforeach()
+  set("${result_variable}" "${result_variable}-NOTFOUND" PARENT_SCOPE)
+endfunction()
 
-# ########################################################################
 # Define an opposite way of specifying an existing option.
 # This may be useful for compatibility.
-# ########################################################################
-macro( option_opposite option opposite )
-  if( DEFINED "${opposite}" )
-    if( ${opposite} )
-      set( "${option}" OFF )
+macro(option_opposite option opposite)
+  if(DEFINED "${opposite}")
+    if(${opposite})
+      set("${option}" OFF)
     else( )
-      set( "${option}" ON )
-    endif( )
-  endif( )
-endmacro( )
+      set("${option}" ON)
+    endif()
+  endif()
+endmacro()
