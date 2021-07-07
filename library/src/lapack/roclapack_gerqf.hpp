e2dfbd3ff97e2c9bd67045a46abdf223d3e0c686
Troy Alderson <58866654+tfalders@users.noreply.github.com>
2021-07-07 15:28:54 -0600

    Implemented gerqf

--- library/src/lapack/roclapack_gerqf.hpp
+++ library/src/lapack/roclapack_gerqf.hpp
@@ -91,5 +91,64 @@ rocblas_status rocsolver_gerqf_template(rocblas_handle handle,
 {
     ROCSOLVER_ENTER("gerqf", "m:", m, "n:", n, "shiftA:", shiftA, "lda:", lda, "bc:", batch_count);
 
-    return rocblas_status_not_implemented;
+    // quick return
+    if(m == 0 || n == 0 || batch_count == 0)
+        return rocblas_status_success;
+
+    hipStream_t stream;
+    rocblas_get_stream(handle, &stream);
+
+    // if the matrix is small, use the unblocked (BLAS-levelII) variant of the
+    // algorithm
+    if(m <= GExQF_GExQ2_SWITCHSIZE || n <= GExQF_GExQ2_SWITCHSIZE)
+        return rocsolver_gerq2_template<T>(handle, m, n, A, shiftA, lda, strideA, ipiv, strideP,
+                                           batch_count, scalars, work_workArr, Abyx_norms_trfact,
+                                           diag_tmptr);
+
+    rocblas_int k = min(m, n); // total number of pivots
+    rocblas_int nb = GExQF_GExQ2_BLOCKSIZE;
+    rocblas_int ki = ((k - GExQF_GExQ2_SWITCHSIZE - 1) / nb) * nb;
+    rocblas_int kk = min(k, ki + nb);
+    rocblas_int jb, j = k - kk + ki;
+    rocblas_int mu = m, nu = n;
+
+    rocblas_int ldw = GEQxF_GEQx2_BLOCKSIZE;
+    rocblas_stride strideW = rocblas_stride(ldw) * ldw;
+
+    while(j >= k - kk)
+    {
+        // Factor diagonal and subdiagonal blocks
+        jb = min(k - j, nb); // number of columns in the block
+        rocsolver_gerq2_template<T>(handle, jb, n - k + j + jb, A, shiftA + idx2D(m - k + j, 0, lda),
+                                    lda, strideA, (ipiv + j), strideP, batch_count, scalars,
+                                    work_workArr, Abyx_norms_trfact, diag_tmptr);
+
+        // apply transformation to the rest of the matrix
+        if(m - k + j > 0)
+        {
+            // compute block reflector
+            rocsolver_larft_template<T>(handle, rocblas_backward_direction, rocblas_row_wise,
+                                        n - k + j + jb, jb, A, shiftA + idx2D(m - k + j, 0, lda),
+                                        lda, strideA, (ipiv + j), strideP, Abyx_norms_trfact, ldw,
+                                        strideW, batch_count, scalars, (T*)work_workArr, workArr);
+
+            // apply the block reflector
+            rocsolver_larfb_template<BATCHED, STRIDED, T>(
+                handle, rocblas_side_right, rocblas_operation_none, rocblas_backward_direction,
+                rocblas_row_wise, m - k + j, n - k + j + jb, jb, A,
+                shiftA + idx2D(m - k + j, 0, lda), lda, strideA, Abyx_norms_trfact, 0, ldw, strideW,
+                A, shiftA, lda, strideA, batch_count, diag_tmptr, workArr);
+        }
+        j -= nb;
+        mu = m - k + j + jb;
+        nu = n - k + j + jb;
+    }
+
+    // factor last block
+    if(mu > 0 && nu > 0)
+        rocsolver_gerq2_template<T>(handle, mu, nu, A, shiftA, lda, strideA, ipiv, strideP,
+                                    batch_count, scalars, work_workArr, Abyx_norms_trfact,
+                                    diag_tmptr);
+
+    return rocblas_status_success;
 }
