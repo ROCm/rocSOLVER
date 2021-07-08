719ad4150032161acb3a3082e0a70da7030243ca
Troy Alderson <58866654+tfalders@users.noreply.github.com>
2021-07-08 14:42:32 -0600

    Move optimized kernels into separate cpp files

---
+++ library/src/include/rocsolver_small_kernels.hpp
@@ -0,0 +1,60 @@
+/* ************************************************************************
+ * Copyright (c) 2021 Advanced Micro Devices, Inc.
+ * ************************************************************************ */
+
+#pragma once
+
+#include "rocblas.hpp"
+#include "rocsolver.h"
+
+/*
+ * ===========================================================================
+ *    common location for declarations of small-size kernel launchers. Small-
+ *    size kernels and their launchers are defined in cpp files to help with
+ *    compile times.
+ * ===========================================================================
+ */
+
+#ifdef OPTIMAL
+
+template <typename T, typename U>
+rocblas_status getf2_run_small(rocblas_handle handle,
+                               const rocblas_int m,
+                               const rocblas_int n,
+                               U A,
+                               const rocblas_int shiftA,
+                               const rocblas_int lda,
+                               const rocblas_stride strideA,
+                               rocblas_int* ipiv,
+                               const rocblas_int shiftP,
+                               const rocblas_stride strideP,
+                               rocblas_int* info,
+                               const rocblas_int batch_count,
+                               const rocblas_int pivot);
+
+template <typename T, typename U>
+rocblas_status getri_run_small(rocblas_handle handle,
+                               const rocblas_int n,
+                               U A,
+                               const rocblas_int shiftA,
+                               const rocblas_int lda,
+                               const rocblas_stride strideA,
+                               rocblas_int* ipiv,
+                               const rocblas_int shiftP,
+                               const rocblas_stride strideP,
+                               rocblas_int* info,
+                               const rocblas_int batch_count,
+                               const bool complete);
+
+template <typename T, typename U>
+void trti2_run_small(rocblas_handle handle,
+                     const rocblas_fill uplo,
+                     const rocblas_diagonal diag,
+                     const rocblas_int n,
+                     U A,
+                     const rocblas_int shiftA,
+                     const rocblas_int lda,
+                     const rocblas_stride strideA,
+                     const rocblas_int batch_count);
+
+#endif // OPTIMAL
