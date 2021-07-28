4f13f56334e26dde4eadc5f0939002db5aea1b85
Juan Zuniga-Anaya <50754207+jzuniga-amd@users.noreply.github.com>
2021-07-28 13:56:49 -0600

    bump version and prepare changelog for new dev cycle (#300)

--- bump_rocsolver_version.sh
+++ bump_rocsolver_version.sh
@@ -3,20 +3,20 @@
 # run this script in develop after merging develop/staging into master at the feature-complete date
 # Edit script to bump versions for new development cycle/release.
 
-OLD_ROCSOLVER_VERSION="3.15.0"  # Distributed with ROCm 4.5
-NEW_ROCSOLVER_VERSION="3.16.0"  # Distributed with ROCm 4.6
+OLD_ROCSOLVER_VERSION="3.16.0"  # Distributed with ROCm 5.0
+NEW_ROCSOLVER_VERSION="3.17.0"  # Distributed with ROCm 5.1
 sed -i "s/${OLD_ROCSOLVER_VERSION}/${NEW_ROCSOLVER_VERSION}/g" CMakeLists.txt
 
 # for documentation
-OLD_ROCSOLVER_DOCS_VERSION="3.15"
-NEW_ROCSOLVER_DOCS_VERSION="3.16"
+OLD_ROCSOLVER_DOCS_VERSION="3.16"
+NEW_ROCSOLVER_DOCS_VERSION="3.17"
 sed -i "s/${OLD_ROCSOLVER_DOCS_VERSION}/${NEW_ROCSOLVER_DOCS_VERSION}/g" docs/source/conf.py
 
 # for rocBLAS package requirements
-OLD_ROCBLAS_VERSION_DOWN="2.41"
-NEW_ROCBLAS_VERSION_DOWN="2.42"
-OLD_ROCBLAS_VERSION_UP="2.42"
-NEW_ROCBLAS_VERSION_UP="2.43"
+OLD_ROCBLAS_VERSION_DOWN="2.42"
+NEW_ROCBLAS_VERSION_DOWN="2.43"
+OLD_ROCBLAS_VERSION_UP="2.43"
+NEW_ROCBLAS_VERSION_UP="2.44"
 sed -i "s/${OLD_ROCBLAS_VERSION_UP}/${NEW_ROCBLAS_VERSION_UP}/g" library/CMakeLists.txt
 sed -i "s/${OLD_ROCBLAS_VERSION_DOWN}/${NEW_ROCBLAS_VERSION_DOWN}/g" library/CMakeLists.txt
 
