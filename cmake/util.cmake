30545c29a4056fdbb9ceb7c2a474427a5ec27082
Cordell Bloor <Cordell.Bloor@amd.com>
2021-08-15 02:30:36 -0600

    Expand get_imported_target_location
    Check the unqualified property, then the property qualified for the
    corresponding configuration, then fall back to all remaining
    configuration properties in the order listed in the options.

--- cmake/util.cmake
+++ cmake/util.cmake
@@ -17,10 +17,14 @@ endfunction()
 
 # Search through the location properties to find one that is set
 function(get_imported_target_location result_variable imported_target)
+  string(TOUPPER "${CMAKE_BUILD_TYPE}" config)
   set(properties_to_search
-    IMPORTED_LOCATION
-    IMPORTED_LOCATION_RELEASE
-    IMPORTED_LOCATION_DEBUG
+    "IMPORTED_LOCATION"
+    "IMPORTED_LOCATION_${config}"
+    "IMPORTED_LOCATION_DEBUG"
+    "IMPORTED_LOCATION_RELEASE"
+    "IMPORTED_LOCATION_RELWITHDEBINFO"
+    "IMPORTED_LOCATION_MINSIZEREL"
   )
   foreach(property ${properties_to_search})
     get_target_property(location "${imported_target}" "${property}")
