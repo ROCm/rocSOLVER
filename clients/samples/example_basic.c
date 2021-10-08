b5fee4c6ebce90b747474e7494a458dfa9c27be1
Cory Bloor <Cordell.Bloor@amd.com>
2021-10-08 14:18:26 -0600

    Update URLs in samples (#321)
    [ROCm/rocSOLVER commit: 6e8410ddc1d397fd7374a09c94a839f88b11241f]

--- clients/samples/example_basic.c
+++ clients/samples/example_basic.c
@@ -31,7 +31,7 @@ double* create_example_matrix(rocblas_int *M_out,
 }
 
 // We use rocsolver_dgeqrf to factor a real M-by-N matrix, A.
-// See https://rocsolver.readthedocs.io/en/latest/userguide_api.html#c.rocsolver_dgeqrf
+// See https://rocsolver.readthedocs.io/en/latest/api_lapackfunc.html#c.rocsolver_dgeqrf
 // and https://www.netlib.org/lapack/explore-html/df/dc5/group__variants_g_ecomputational_ga3766ea903391b5cf9008132f7440ec7b.html
 int main() {
   rocblas_int M;          // rows
