ece863d0acd4e493b85ed4889d29a31b6839a0c4
Troy Alderson <58866654+tfalders@users.noreply.github.com>
2022-06-06 16:40:03 -0600

    Increased test tolerance

--- clients/include/testing_sygvj_hegvj.hpp
+++ clients/include/testing_sygvj_hegvj.hpp
@@ -730,9 +730,9 @@ void testing_sygvj_hegvj(Arguments& argus)
     }
 
     // validate results for rocsolver-test
-    // using n * machine_precision as tolerance
+    // using 2 * n * machine_precision as tolerance
     if(argus.unit_check)
-        ROCSOLVER_TEST_CHECK(T, max_error, n);
+        ROCSOLVER_TEST_CHECK(T, max_error, 2 * n);
 
     // output results for rocsolver-bench
     if(argus.timing)
