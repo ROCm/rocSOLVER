82b4e43679e818381399afaf2928dfc403ac522a
Cordell Bloor <Cordell.Bloor@amd.com>
2022-12-15 17:03:03 -0700

    Drop padding checks from d_vector

--- clients/rocblascommon/d_vector.hpp
+++ clients/rocblascommon/d_vector.hpp
@@ -30,25 +30,11 @@ public:
         return size;
     }
 
-#ifdef ROCSOLVER_CLIENTS_TEST
-    U guard[PAD];
-    d_vector(size_t s)
-        : size(s)
-        , bytes((s + PAD * 2) * sizeof(T))
-    {
-        // Initialize guard with random data
-        if(PAD > 0)
-        {
-            rocblas_init_nan(guard, PAD);
-        }
-    }
-#else
     d_vector(size_t s)
         : size(s)
         , bytes(s ? s * sizeof(T) : sizeof(T))
     {
     }
-#endif
 
     T* device_vector_setup()
     {
@@ -58,78 +44,17 @@ public:
             fmt::print(stderr, "Error allocating {} bytes ({} GB)\n", bytes, bytes >> 30);
             d = nullptr;
         }
-#ifdef ROCSOLVER_CLIENTS_TEST
-        else
-        {
-            if(PAD > 0)
-            {
-                // Copy guard to device memory before allocated memory
-                THROW_IF_HIP_ERROR(hipMemcpy(d, guard, sizeof(guard), hipMemcpyHostToDevice));
-
-                // Point to allocated block
-                d += PAD;
-
-                // Copy guard to device memory after allocated memory
-                THROW_IF_HIP_ERROR(hipMemcpy(d + size, guard, sizeof(guard), hipMemcpyHostToDevice));
-            }
-        }
-#endif
         return d;
     }
 
-    void device_vector_check(T* d)
-    {
-#ifdef ROCSOLVER_CLIENTS_TEST
-        if(PAD > 0)
-        {
-            U host[PAD];
-
-            // Copy device memory after allocated memory to host
-            THROW_IF_HIP_ERROR(hipMemcpy(host, d + this->size, sizeof(guard), hipMemcpyDeviceToHost));
-
-            // Make sure no corruption has occurred
-            EXPECT_EQ(memcmp(host, guard, sizeof(guard)), 0);
-
-            // Point to guard before allocated memory
-            d -= PAD;
-
-            // Copy device memory after allocated memory to host
-            THROW_IF_HIP_ERROR(hipMemcpy(host, d, sizeof(guard), hipMemcpyDeviceToHost));
-
-            // Make sure no corruption has occurred
-            EXPECT_EQ(memcmp(host, guard, sizeof(guard)), 0);
-        }
-#endif
-    }
+    void device_vector_check(T* d) {}
 
     void device_vector_teardown(T* d)
     {
         if(d != nullptr)
         {
-#ifdef ROCSOLVER_CLIENTS_TEST
-            if(PAD > 0)
-            {
-                U host[PAD];
-
-                // Copy device memory after allocated memory to host
-                THROW_IF_HIP_ERROR(
-                    hipMemcpy(host, d + this->size, sizeof(guard), hipMemcpyDeviceToHost));
-
-                // Make sure no corruption has occurred
-                EXPECT_EQ(memcmp(host, guard, sizeof(guard)), 0);
-
-                // Point to guard before allocated memory
-                d -= PAD;
-
-                // Copy device memory after allocated memory to host
-                THROW_IF_HIP_ERROR(hipMemcpy(host, d, sizeof(guard), hipMemcpyDeviceToHost));
-
-                // Make sure no corruption has occurred
-                EXPECT_EQ(memcmp(host, guard, sizeof(guard)), 0);
-            }
-#endif
             // Free device memory
-            THROW_IF_HIP_ERROR((hipFree)(d));
+            CHECK_HIP_ERROR((hipFree)(d));
         }
     }
 };
