5a0440043b0505b8b9505a74c331f5e4540cb543
Cordell Bloor <Cordell.Bloor@amd.com>
2023-01-26 14:15:58 -0700

    Fix formatting

--- clients/include/testing_syevx_heevx.hpp
+++ clients/include/testing_syevx_heevx.hpp
@@ -379,7 +379,7 @@ void syevx_heevx_getError(const rocblas_handle handle,
                 err = 0;
                 for(int j = 0; j < hinfo[b][0]; j++)
                 {
-                    EXPECT_NE(hIfailRes[b][j], 0) << "where b = " << b  << ", j = " << j;
+                    EXPECT_NE(hIfailRes[b][j], 0) << "where b = " << b << ", j = " << j;
                     if(hIfailRes[b][j] == 0)
                         err++;
                 }
