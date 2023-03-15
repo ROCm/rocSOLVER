6a4d618d4e6ae4cbc4a78bb17f3b25927e7dd0a3
Cordell Bloor <Cordell.Bloor@amd.com>
2023-03-15 15:34:02 +0000

    Fix build without rocsparse

--- library/src/refact/rocrefact_no_rocsparse.cpp
+++ library/src/refact/rocrefact_no_rocsparse.cpp
@@ -2,7 +2,7 @@
  * Copyright (c) 2023 Advanced Micro Devices, Inc.
  * ************************************************************************ */
 
-#include <rocblas/rocblas.h>
+#include "rocsolver/rocsolver.h"
 
 /* The rocsolver library maintains binary compatibility when built without rocsparse,
    but the functions that depend on rocsparse functionality will return
@@ -11,12 +11,12 @@
 
 extern "C" {
 
-rocblas_status rocsolver_rfinfo_create(rocsolver_rfinfo*, rocblas_handle)
+rocblas_status rocsolver_create_rfinfo(rocsolver_rfinfo*, rocblas_handle)
 {
     return rocblas_status_not_implemented;
 }
 
-rocblas_status rocsolver_rfinfo_destroy(rocsolver_rfinfo)
+rocblas_status rocsolver_destroy_rfinfo(rocsolver_rfinfo)
 {
     return rocblas_status_not_implemented;
 }
@@ -151,6 +151,38 @@ rocblas_status rocsolver_dcsrrf_sumlu(rocblas_handle,
                                       rocblas_int*,
                                       rocblas_int*,
                                       double*)
+{
+    return rocblas_status_not_implemented;
+}
+
+rocblas_status rocsolver_scsrrf_solve(rocblas_handle,
+                                      const rocblas_int,
+                                      const rocblas_int,
+                                      const rocblas_int,
+                                      rocblas_int*,
+                                      rocblas_int*,
+                                      float*,
+                                      rocblas_int*,
+                                      rocblas_int*,
+                                      rocsolver_rfinfo,
+                                      float*,
+                                      const rocblas_int)
+{
+    return rocblas_status_not_implemented;
+}
+
+rocblas_status rocsolver_dcsrrf_solve(rocblas_handle,
+                                      const rocblas_int,
+                                      const rocblas_int,
+                                      const rocblas_int,
+                                      rocblas_int*,
+                                      rocblas_int*,
+                                      double*,
+                                      rocblas_int*,
+                                      rocblas_int*,
+                                      rocsolver_rfinfo,
+                                      double*,
+                                      const rocblas_int)
 {
     return rocblas_status_not_implemented;
 }
