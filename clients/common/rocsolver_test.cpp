98f9a597090b113e5c1960c91b0c82dd101968ea
Cordell Bloor <Cordell.Bloor@amd.com>
2023-08-01 02:28:38 -0600

    Fix stack overflow in sparse tests on Windows
    It appears that the while(p != p_parent) check would create an infinite
    loop on Windows, ultimately resulting in a stack overflow. With the
    addition of exe-relative lookups, all other searches are superfluous
    and can be removed.

---
+++ clients/common/rocsolver_test.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************
+ * Copyright (c) 2023 Advanced Micro Devices, Inc.
+ * ************************************************************************ */
+
+#include <cstdlib>
+
+#include <fmt/core.h>
+#include <fmt/ostream.h>
+#include <fmt/ranges.h>
+
+#include "rocblascommon/clients_utility.hpp"
+#include "rocsolver_test.hpp"
+
+fs::path get_sparse_data_dir()
+{
+    // first check an environment variable
+    if(const char* datadir = std::getenv("ROCSOLVER_TEST_DATA"))
+        return fs::path{datadir};
+
+    std::vector<std::string> considered;
+
+    // check relative to the running executable
+    fs::path exe_path = fs::path(rocsolver_exepath());
+    std::vector<fs::path> candidates = {"../share/rocsolver/test", "../../../../clients/sparsedata"};
+    for(const fs::path& candidate : candidates)
+    {
+        fs::path exe_relative = fs::weakly_canonical(exe_path / candidate);
+        if(fs::exists(exe_relative))
+            return exe_relative;
+        considered.push_back(exe_relative.string());
+    }
+
+    fmt::print(stderr,
+               "Warning: default sparse data directories not found. "
+               "Defaulting to current working directory.\nExecutable location: {}\n"
+               "Paths considered:\n{}\n",
+               exe_path.string(), fmt::join(considered, "\n"));
+
+    return fs::current_path();
+}
