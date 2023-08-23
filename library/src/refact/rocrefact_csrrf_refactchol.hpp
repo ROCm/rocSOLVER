5cf26aa6f470a21d46718b52892a17f263049d12
Troy Alderson <58866654+tfalders@users.noreply.github.com>
2023-08-22 20:23:38 -0600

    Fix failing tests

--- library/src/refact/rocrefact_csrrf_refactchol.hpp
+++ library/src/refact/rocrefact_csrrf_refactchol.hpp
@@ -13,92 +13,89 @@
 
 // -------------------------------------------
 // Compute B = beta * B + alpha * (Q * A * Q') as
-//
-// NOTE: access ONLY the  LOWER triangular part matrix A and matrix B
-//
-// Compute B += alpha * (Q * A * Q')
-// where sparsity pattern of reordered (Q * A * Q') is a proper subset
+// (1) B = beta * B
+// (2) B += alpha * (Q * A * Q')
+// where sparsity pattern of reordered A is a proper subset
 // of sparsity pattern of B.
 // Further assume for each row, the column indices are
 // in increasing sorted order
 // -------------------------------------------
 template <typename T>
 ROCSOLVER_KERNEL void rf_add_QAQ_kernel(const rocblas_int n,
-                                        rocblas_int* Qold2new,
+                                        rocblas_int* pivQ,
+                                        rocblas_int* inv_pivQ,
                                         const T alpha,
                                         rocblas_int* Ap,
                                         rocblas_int* Ai,
                                         T* Ax,
-                                        rocblas_int* Bp,
-                                        rocblas_int* Bi,
-                                        T* Bx)
+                                        const T beta,
+                                        rocblas_int* LUp,
+                                        rocblas_int* LUi,
+                                        T* LUx)
 {
-    // ------------------------------------------------------
-    // Note: access to ONLY lower triangular part of matrix A
-    // to update ONLY lower triangular part of matrix B
-    // ------------------------------------------------------
+    rocblas_int tix = hipBlockIdx_x * hipBlockDim_x + hipThreadIdx_x;
+    rocblas_int tiy = hipThreadIdx_y;
 
-    rocblas_int const tix = hipBlockIdx_x * hipBlockDim_x + hipThreadIdx_x;
-    rocblas_int const tiy = hipThreadIdx_y;
-
-    // ----------------------------------------------
-    // each row is processed by one "wave" of threads
-    // ----------------------------------------------
+    // -------------------------------------------
+    // If Q is NULL, then treat as identity permutation
+    // -------------------------------------------
     if(tix < n)
     {
-        rocblas_int const irow_old = tix;
-        rocblas_int const istart_old = Ap[irow_old];
-        rocblas_int const iend_old = Ap[irow_old + 1];
+        rocblas_int irow = tix;
+        rocblas_int istart = LUp[irow];
+        rocblas_int iend = LUp[irow + 1];
+        rocblas_int i, icol;
 
-        for(rocblas_int i = istart_old + tiy; i < iend_old; i += hipBlockDim_y)
+        rocblas_int irow_old = (pivQ ? pivQ[irow] : irow);
+        rocblas_int istart_old = Ap[irow_old];
+        rocblas_int iend_old = Ap[irow_old + 1];
+        rocblas_int i_old, icol_old;
+
+        T aij;
+
+        // ----------------
+        // scale B by beta
+        // ----------------
+        for(i = istart + tiy; i < iend; i += hipBlockDim_y)
         {
-            // ---------------------------------------------------
-            // Note: access only lower triangular part of matrix A
-            // ---------------------------------------------------
-
-            rocblas_int const jcol_old = Ai[i];
-            bool const is_strictly_upper_A = (irow_old < jcol_old);
-            if(is_strictly_upper_A)
-            {
+            // only access lower triangle of B
+            if(irow < LUi[i])
                 break;
+            LUx[i] *= beta;
+        }
+        __syncthreads();
+
+        // ------------------------------
+        // scale A by alpha and add to B
+        // ------------------------------
+        for(i_old = istart_old + tiy; i_old < iend_old; i_old += hipBlockDim_y)
+        {
+            icol_old = Ai[i_old];
+            icol = (inv_pivQ ? inv_pivQ[icol_old] : icol_old);
+
+            // only access lower triangle of A
+            if(irow_old < icol_old)
+                break;
+
+            // upper part of QAQ' is conjugate transpose of lower part
+            if(irow < icol)
+            {
+                aij = conj(Ax[i_old]);
+                istart = LUp[icol];
+                iend = LUp[icol + 1];
+                icol = irow;
+            }
+            else
+            {
+                aij = Ax[i_old];
+                istart = LUp[irow];
+                iend = LUp[irow + 1];
             }
 
-            T aij = Ax[i];
-
-            // --------------------------------------------
-            // Note: access only lower triangular part of B
-            // --------------------------------------------
-
-            rocblas_int irow_new = Qold2new[irow_old];
-            rocblas_int jcol_new = Qold2new[jcol_old];
-            bool const is_strictly_upper_B = (irow_new < jcol_new);
-
-            if(is_strictly_upper_B)
+            i = rf_search<T>(LUi, istart, iend, icol);
+            if(i != -1)
             {
-                // ------------------------------------------
-                // take the conj transpose to access ONLY
-                // the LOWER triangular part of B
-                // ------------------------------------------
-                aij = conj(aij);
-
-                // --------------------------
-                // swap( irow_new, jcol_new )
-                // --------------------------
-                rocblas_int const iswap = irow_new;
-                irow_new = jcol_new;
-                jcol_new = iswap;
-            }
-
-            // ----------------------------
-            // search for entry B(irow_new, jcol_new)
-            // ----------------------------
-            rocblas_int const istart_new = Bp[irow_new];
-            rocblas_int const iend_new = Bp[irow_new + 1];
-            rocblas_int const ipos = rf_search<T>(Bi, istart_new, iend_new, jcol_new);
-            bool const is_found = (istart_new <= ipos) && (ipos < iend_new);
-            if(is_found)
-            {
-                Bx[ipos] += alpha * aij;
+                LUx[i] += alpha * aij;
             }
         }
     }
@@ -197,20 +194,16 @@ rocblas_status rocsolver_csrrf_refactchol_template(rocblas_handle handle,
     ROCSOLVER_LAUNCH_KERNEL(rf_ipvec_kernel<T>, dim3(nblocks), dim3(BS2), 0, stream, n, pivQ,
                             (rocblas_int*)work);
 
-    // --------------------------------------------------
-    // set valT[] to zero  before numerical factorization
-    // --------------------------------------------------
-    if(hipMemsetAsync((void*)valT, 0, sizeof(T) * nnzT, stream) != hipSuccess)
-        return rocblas_status_internal_error;
-
     // --------------------------------------------------------------
     // copy Q'*A*Q into T
     //
-    // Note: assume A and T are symmetric and ONLY the LOWER triangular parts of A, and T are touched
+    // Note: assume A and B are symmetric and ONLY the LOWER triangular parts of A and T are touched
     // --------------------------------------------------------------
     T const alpha = static_cast<T>(1);
+    T const beta = static_cast<T>(0);
     ROCSOLVER_LAUNCH_KERNEL(rf_add_QAQ_kernel<T>, dim3(nblocks, 1), dim3(BS2, BS2), 0, stream, n,
-                            pivQ, alpha, ptrA, indA, valA, ptrT, indT, valT);
+                            pivQ, (rocblas_int*)work, alpha, ptrA, indA, valA, beta, ptrT, indT,
+                            valT);
 
     // perform incomplete factorization of T
     ROCSPARSE_CHECK(rocsparseCall_csric0(rfinfo->sphandle, n, nnzT, rfinfo->descrT, valT, ptrT,
