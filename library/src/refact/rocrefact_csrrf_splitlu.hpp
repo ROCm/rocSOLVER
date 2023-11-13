5683cf35109fcadd279b0328aaee4ac82fb139d9
Eduardo D'Azevedo <Eduardo.DAzevedo@amd.com>
2023-11-13 17:58:36 -0500

    use rocthrust for prefix sum

--- library/src/refact/rocrefact_csrrf_splitlu.hpp
+++ library/src/refact/rocrefact_csrrf_splitlu.hpp
@@ -27,12 +27,143 @@
 
 #pragma once
 
+#include "thrust/device_ptr.h"
+#include "thrust/device_vector.h"
+#include "thrust/fill.h"
+#include "thrust/host_vector.h"
+#include "thrust/scan.h"
+#include "thrust/sequence.h"
 #include <iostream>
 
 #include "rocblas.hpp"
 #include "rocsolver/rocsolver.h"
 #include "rocsparse.hpp"
 
+template <typename T>
+ROCSOLVER_KERNEL void rf_splitLU_gen_nzLU_kernel(const rocblas_int n,
+                                                 const rocblas_int nnzM,
+                                                 rocblas_int* Mp,
+                                                 rocblas_int* Mi,
+                                                 rocblas_int* nzLarray,
+                                                 rocblas_int* nzUarray)
+{
+    const auto avg_nnzM = max(1, nnzM / n);
+
+    const auto waveSize = (avg_nnzM >= warpSize) ? warpSize
+        : (avg_nnzM >= (warpSize / 2))           ? (warpSize / 2)
+        : (avg_nnzM >= (warpSize / 4))           ? (warpSize / 4)
+        : (avg_nnzM >= (warpSize / 8))           ? (warpSize / 8)
+        : (avg_nnzM >= (warpSize / 16))          ? (warpSize / 16)
+                                                 : 1;
+
+    const auto nthreads = gridDim.x * blockDim.x;
+    const auto nwaves = nthreads / waveSize;
+
+    const auto tid = threadIdx.x + blockIdx.x * blockDim.x;
+    const auto lid = tid % waveSize;
+    const auto wid = tid / waveSize;
+
+    for(auto irow = wid; irow < n; irow += nwaves)
+    {
+        const auto kstart = Mp[irow];
+        const auto kend = Mp[irow + 1];
+        bool is_found = false;
+        for(auto k = kstart + lid; (k < kend) && (!is_found); k += waveSize)
+        {
+            const auto icol = Mi[k];
+            if(icol == irow)
+            {
+                is_found = true;
+                const auto kdiag = k;
+                nzUarray[irow] = kend - kdiag;
+                nzLarray[irow] = (kdiag - kstart);
+                nzLarray[irow] += 1; // add 1 for unit diagonal
+            };
+        };
+    };
+}
+
+template <typename T>
+ROCSOLVER_KERNEL void rf_splitLU_copy_kernel(const rocblas_int n,
+                                             const rocblas_int nnzM,
+                                             rocblas_int* Mp,
+                                             rocblas_int* Mi,
+                                             T* Mx,
+                                             rocblas_int* Lp,
+                                             rocblas_int* Li,
+                                             T* Lx,
+                                             rocblas_int* Up,
+                                             rocblas_int* Ui,
+                                             T* Ux)
+{
+    const auto avg_nnzM = max(1, nnzM / n);
+
+    const auto waveSize = (avg_nnzM >= warpSize) ? warpSize
+        : (avg_nnzM >= (warpSize / 2))           ? (warpSize / 2)
+        : (avg_nnzM >= (warpSize / 4))           ? (warpSize / 4)
+        : (avg_nnzM >= (warpSize / 8))           ? (warpSize / 8)
+        : (avg_nnzM >= (warpSize / 16))          ? (warpSize / 16)
+                                                 : 1;
+
+    const auto nthreads = gridDim.x * blockDim.x;
+    const auto nwaves = nthreads / waveSize;
+
+    const auto tid = threadIdx.x + blockIdx.x * blockDim.x;
+    const auto lid = tid % waveSize;
+    const auto wid = tid / waveSize;
+
+    for(auto irow = wid; irow < n; irow += nwaves)
+    {
+        const auto kstart = Mp[irow];
+        const auto kend = Mp[irow + 1];
+
+        const auto nzU = (Up[irow + 1] - Up[irow]);
+        const auto nzL = (kend - kstart) - nzU;
+        const auto kdiag = kstart + nzL;
+
+        // --------------------------
+        // copy lower triangular part
+        // --------------------------
+        for(auto k = lid; k < nzL; k += waveSize)
+        {
+            const auto kp = kstart + k;
+            const auto icol = Mi[kp];
+            const auto aij = Mx[kp];
+
+            const auto ip = Lp[irow] + k;
+            Li[ip] = icol;
+            Lx[ip] = aij;
+        };
+
+        // --------------------------
+        // copy upper triangular part
+        // --------------------------
+        for(auto k = lid; k < nzU; k += waveSize)
+        {
+            const auto kp = kdiag + k;
+            const auto icol = Mi[kp];
+            const auto aij = Mx[kp];
+
+            const auto ip = Up[irow] + k;
+            Ui[ip] = icol;
+            Ux[ip] = aij;
+        };
+
+        // -------------------
+        // unit diagonal entry of L
+        // -------------------
+        if(lid == 0)
+        {
+            const auto ip = Lp[irow + 1] - 1;
+            Li[ip] = irow;
+            Lx[ip] = static_cast<T>(1);
+        };
+    };
+}
+
+// ----------------------------------------------
+// Note: intended for execution on a single block
+// ----------------------------------------------
 template <typename T>
 ROCSOLVER_KERNEL void rf_splitLU_kernel(const rocblas_int n,
                                         const rocblas_int nnzM,
@@ -47,14 +178,19 @@ ROCSOLVER_KERNEL void rf_splitLU_kernel(const rocblas_int n,
                                         T* Ux,
                                         rocblas_int* work)
 {
+    if(blockIdx.x != 0)
+    {
+        return;
+    };
+
     const rocblas_int avg_nnzM = max(1, nnzM / n);
 
-    const rocblas_int waveSize = (avg_nnzM > warpSize) ? warpSize
-        : (avg_nnzM > warpSize / 2)                    ? warpSize / 2
-        : (avg_nnzM > warpSize / 4)                    ? warpSize / 4
-        : (avg_nnzM > warpSize / 8)                    ? warpSize / 8
-        : (avg_nnzM > warpSize / 16)                   ? warpSize / 16
-                                                       : 1;
+    const rocblas_int waveSize = (avg_nnzM >= warpSize) ? warpSize
+        : (avg_nnzM >= warpSize / 2)                    ? warpSize / 2
+        : (avg_nnzM >= warpSize / 4)                    ? warpSize / 4
+        : (avg_nnzM >= warpSize / 8)                    ? warpSize / 8
+        : (avg_nnzM >= warpSize / 16)                   ? warpSize / 16
+                                                        : 1;
 
     const rocblas_int nthreads = blockDim.x;
     const rocblas_int nwaves = nthreads / waveSize;
@@ -77,7 +213,6 @@ ROCSOLVER_KERNEL void rf_splitLU_kernel(const rocblas_int n,
         const rocblas_int istart = Mp[irow];
         const rocblas_int iend = Mp[irow + 1];
 
-#pragma unroll
         for(auto i = istart + lid; i < iend; i += waveSize)
         {
             const auto icol = Mi[i];
@@ -278,10 +413,46 @@ rocblas_status rocsolver_csrrf_splitlu_template(rocblas_handle handle,
         return rocblas_status_success;
     }
 
-    rocblas_int const nthreads = BS1;
-    rocblas_int const nblocks = 1;
-    ROCSOLVER_LAUNCH_KERNEL(rf_splitLU_kernel<T>, dim3(nblocks), dim3(nthreads), 0, stream, n, nnzT,
-                            ptrT, indT, valT, ptrL, indL, valL, ptrU, indU, valU, work);
+    bool const use_alg1 = false;
+    if(use_alg1)
+    {
+        rocblas_int const nthreads = BS1;
+        rocblas_int const nblocks = 1;
+        ROCSOLVER_LAUNCH_KERNEL(rf_splitLU_kernel<T>, dim3(nblocks), dim3(nthreads), 0, stream, n,
+                                nnzT, ptrT, indT, valT, ptrL, indL, valL, ptrU, indU, valU, work);
+    }
+    else
+    {
+        rocblas_int const nthreads = BS1;
+        rocblas_int const nblocks = max(1, (n - 1) / nthreads + 1);
+
+        rocblas_int* const Lp = ptrL;
+        rocblas_int* const Up = ptrU;
+        // -----------------------------------------
+        // setup number of nonzeros in each row of L
+        // -----------------------------------------
+        ROCSOLVER_LAUNCH_KERNEL(rf_splitLU_gen_nzLU_kernel<T>, dim3(nblocks), dim3(nthreads), 0,
+                                stream, n, nnzT, ptrT, indT, Lp + 1, Up + 1);
+
+        // -------------------------------------
+        // generate prefix sum for Lp[] and Up[]
+        // -------------------------------------
+        auto exec = thrust::hip::par.on(stream);
+
+        thrust::device_ptr<rocblas_int> dev_Lp(Lp);
+        thrust::device_ptr<rocblas_int> dev_Up(Up);
+
+        thrust::inclusive_scan(exec, (dev_Lp + 1), (dev_Lp + 1) + n, (dev_Lp + 1));
+        thrust::inclusive_scan(exec, (dev_Up + 1), (dev_Up + 1) + n, (dev_Up + 1));
+        thrust::fill(exec, dev_Up, dev_Up + 1, 0);
+        thrust::fill(exec, dev_Lp, dev_Lp + 1, 0);
+
+        // -----------------
+        // copy into L and U
+        // -----------------
+        ROCSOLVER_LAUNCH_KERNEL(rf_splitLU_copy_kernel<T>, dim3(nblocks), dim3(nthreads), 0, stream,
+                                n, nnzT, ptrT, indT, valT, Lp, indL, valL, Up, indU, valU);
+    };
 
     return rocblas_status_success;
 }
