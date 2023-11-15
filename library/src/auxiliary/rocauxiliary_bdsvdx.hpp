d5eaffdd0d1474834108787607f9110f7126f9dc
Troy Alderson <58866654+tfalders@users.noreply.github.com>
2023-11-15 11:40:06 -0700

    Relax array length requirements for gesvdx (#625)
    * Use nsv in gesvdx when srange is index
    
    * Relaxed memory requirements for U and V
    
    * Reduced memory requirements for S
    
    * Updated documentation
    
    * Updated bdsvdx test and reduced size of tmpZ
    
    * Minor test fix

--- library/src/auxiliary/rocauxiliary_bdsvdx.hpp
+++ library/src/auxiliary/rocauxiliary_bdsvdx.hpp
@@ -4,7 +4,7 @@
  *     Univ. of Tennessee, Univ. of California Berkeley,
  *     Univ. of Colorado Denver and NAG Ltd..
  *     December 2016
- * Copyright (C) 2016 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (C) 2016-2023 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -94,6 +94,7 @@ ROCSOLVER_KERNEL void bdsvdx_reorder_vect(const rocblas_fill uplo,
 
     for(i = tid; i < nsv; i += hipBlockDim_x)
         S[i] = -work[i];
+    __syncthreads();
 
     for(j = 0; j < nsv; j++)
     {
