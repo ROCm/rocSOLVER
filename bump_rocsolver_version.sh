ce3c24ab1939ad50c70dc3b3687cd01c62176063
Juan Zuniga-Anaya <50754207+jzuniga-amd@users.noreply.github.com>
2023-11-23 10:05:50 -0700

    bump versions and prepare changelog for new dev cycle (#633)
    update versions

--- bump_rocsolver_version.sh
+++ bump_rocsolver_version.sh
@@ -4,12 +4,12 @@
 # Edit script to bump versions for new development cycle/release.
 
 # for rocSOLVER version string
-OLD_ROCSOLVER_VERSION="3.25.0"
-NEW_ROCSOLVER_VERSION="3.26.0"
+OLD_ROCSOLVER_VERSION="3.26.0"
+NEW_ROCSOLVER_VERSION="3.27.0"
 sed -i "s/${OLD_ROCSOLVER_VERSION}/${NEW_ROCSOLVER_VERSION}/g" CMakeLists.txt
 
 # for rocBLAS package requirements
-OLD_ROCBLAS_VERSION="4.1"
-NEW_ROCBLAS_VERSION="4.2"
+OLD_ROCBLAS_VERSION="4.2"
+NEW_ROCBLAS_VERSION="4.3"
 sed -i "s/${OLD_ROCBLAS_VERSION}/${NEW_ROCBLAS_VERSION}/g" CMakeLists.txt
 
