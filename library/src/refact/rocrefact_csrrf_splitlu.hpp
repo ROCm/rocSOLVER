09c5236e07f723d3a5e2f1d158bfd6d9ed303fdb
Eduardo D'Azevedo <Eduardo.DAzevedo@amd.com>
2024-01-06 13:10:41 -0500

    remove #undef HIP_CHECK

--- library/src/refact/rocrefact_csrrf_splitlu.hpp
+++ library/src/refact/rocrefact_csrrf_splitlu.hpp
@@ -1,5 +1,5 @@
 /* **************************************************************************
- * Copyright (C) 2023 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (C) 2023-2024 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -499,4 +499,3 @@ rocblas_status rocsolver_csrrf_splitlu_template(rocblas_handle handle,
 
     return rocblas_status_success;
 }
-#undef HIP_CHECK
