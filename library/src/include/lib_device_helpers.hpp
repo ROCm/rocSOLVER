0a4bcae832c2333b0fa88c093809e8645101c15a
Eduardo D'Azevedo <Eduardo.DAzevedo@amd.com>
2024-01-26 20:53:58 -0500

    reorder swap routines

--- library/src/include/lib_device_helpers.hpp
+++ library/src/include/lib_device_helpers.hpp
@@ -798,10 +798,89 @@ __device__ static void shell_sort_ascending(const I n, S* a, I* map = nullptr)
         return;
     };
 
-    // ------------------------
-    // Shell sort (from wikipedia)
-    // Sort an array a[0...(n-1)].
-    // ------------------------
+    bool const is_root_thread
+        = (hipThreadIdx_x == 0) && (hipThreadIdx_y == 0) && (hipThreadIdx_z == 0);
+
+    int constexpr ngaps = 8;
+    int const gaps[ngaps] = {701, 301, 132, 57, 23, 10, 4, 1}; // # Ciura gap sequence
+
+    auto const k_start = hipThreadIdx_x + hipThreadIdx_y * hipBlockDim_x
+        + hipThreadIdx_z * (hipBlockDim_x * hipBlockDim_y);
+
+    auto const k_inc = (hipBlockDim_x * hipBlockDim_y) * hipBlockDim_z;
+
+    bool const has_map = (map != nullptr);
+
+    __syncthreads();
+    if(has_map)
+    {
+        for(auto k = k_start; k < n; k += k_inc)
+        {
+            map[k] = k;
+        };
+    };
+    __syncthreads();
+
+    if(is_root_thread)
+    {
+        for(auto igap = 0; igap < ngaps; igap++)
+        {
+            auto const gap = gaps[igap];
+
+            for(I i = gap; i < n; i += 1)
+            {
+                {
+                    S const temp = a[i];
+                    auto const itemp = (has_map) ? map[i] : 0;
+
+                    auto j = i;
+
+                    while((j >= gap) && (a[j - gap] > temp))
+                    {
+                        a[j] = a[j - gap];
+                        if(has_map)
+                        {
+                            map[j] = map[j - gap];
+                        };
+                        j -= gap;
+                    };
+
+                    a[j] = temp;
+                    if(has_map)
+                    {
+                        map[j] = itemp;
+                    };
+                };
+            };
+        };
+    };
+    __syncthreads();
+#ifdef NDEBUG
+#else
+    if(n >= 2)
+    {
+        for(auto k = k_start; k < (n - 1); k += k_inc)
+        {
+            assert(a[k] <= a[k + 1]);
+        };
+    };
+    __syncthreads();
+#endif
+}
+
+template <typename S, typename I>
+__device__ static void shell_sort_descending(const I n, S* a, I* map = nullptr)
+{
+    // -----------------------------------------------
+    // Sort array a[0...(n-1)] and generate permutation vector
+    // in map[] if map[] is available
+    // Note: performs in a single thread block
+    // -----------------------------------------------
+
+    if((n <= 0) || (a == nullptr))
+    {
+        return;
+    };
 
     bool const is_root_thread
         = (hipThreadIdx_x == 0) && (hipThreadIdx_y == 0) && (hipThreadIdx_z == 0);
@@ -826,38 +905,21 @@ __device__ static void shell_sort_ascending(const I n, S* a, I* map = nullptr)
     };
     __syncthreads();
 
-    // ---------------------------------------------------------------
-    // perform sorting to generate permutation vector using shell sort
-    // ---------------------------------------------------------------
     if(is_root_thread)
     {
-        // --------------------------------------------------------------------------
-        // Start with the largest gap and work down to a gap of 1
-        // similar to insertion sort but instead of 1, gap is being used in each step
-        // --------------------------------------------------------------------------
         for(auto igap = 0; igap < ngaps; igap++)
         {
             auto const gap = gaps[igap];
 
-            // -----------------------------------------------------
-            // Do a gapped insertion sort for every elements in gaps
-            // Each loop leaves a[0..gap-1] in gapped order
-            // -----------------------------------------------------
             for(rocblas_int i = gap; i < n; i += 1)
             {
                 {
-                    // -----------------------------------------------
-                    // save a[i] in temp and make a hole at position i
-                    // -----------------------------------------------
                     S const temp = a[i];
                     auto const itemp = (has_map) ? map[i] : 0;
 
-                    // ---------------------------------------------------------------------------------
-                    // shift earlier gap-sorted elements up until the correct location for a[i] is found
-                    // ---------------------------------------------------------------------------------
                     auto j = i;
 
-                    while((j >= gap) && (a[j - gap] > temp))
+                    while((j >= gap) && (a[j - gap] < temp))
                     {
                         a[j] = a[j - gap];
                         if(has_map)
@@ -867,9 +929,6 @@ __device__ static void shell_sort_ascending(const I n, S* a, I* map = nullptr)
                         j -= gap;
                     };
 
-                    // ----------------------------------------------------
-                    // put temp (the original a[i]) in its correct location
-                    // ----------------------------------------------------
                     a[j] = temp;
                     if(has_map)
                     {
@@ -886,11 +945,29 @@ __device__ static void shell_sort_ascending(const I n, S* a, I* map = nullptr)
     {
         for(auto k = k_start; k < (n - 1); k += k_inc)
         {
-            assert(a[k] <= a[k + 1]);
+            assert(a[k] >= a[k + 1]);
         };
     };
     __syncthreads();
 #endif
+}
+
+template <typename S, typename I>
+__device__ static void shell_sort(const I n, S* a, I* map = nullptr, const bool is_ascending = true)
+{
+    if((n <= 0) || (a == nullptr))
+    {
+        return;
+    };
+
+    if(is_ascending)
+    {
+        shell_sort_ascending(n, a, map);
+    }
+    else
+    {
+        shell_sort_descending(n, a, map);
+    };
 }
 
 template <typename S, typename I>
@@ -975,72 +1052,6 @@ __device__ static void selection_sort_ascending(const I n, S* D, I* map = nullpt
     };
     __syncthreads();
 #endif
-}
-
-template <typename T, typename I>
-__device__ static void permute_swap(const I n, T* C, I ldc, I* map)
-{
-    // --------------------------------------------
-    // perform swaps to implement permutation vector
-    // the permutation vector will be restored to the
-    // identity permutation 0,1,2,...
-    //
-    // Note: performs in a thread block
-    // --------------------------------------------
-    {
-        bool const has_work = (n >= 1) && (C != nullptr) && (ldc >= 1) && (map != nullptr);
-        if(!has_work)
-        {
-            return;
-        };
-    };
-
-    auto const tid = hipThreadIdx_x + hipThreadIdx_y * hipBlockDim_x
-        + hipThreadIdx_z * (hipBlockDim_x * hipBlockDim_y);
-
-    auto const nthreads = (hipBlockDim_x * hipBlockDim_y) * hipBlockDim_z;
-
-    auto const k_start = tid;
-    auto const k_inc = nthreads;
-
-    bool const is_root_thread = (tid == 0);
-
-    for(I i = 0; i < n; i++)
-    {
-        __syncthreads();
-
-        while(map[i] != i)
-        {
-            auto const map_i = map[i];
-            auto const map_ii = map[map[i]];
-
-            __syncthreads();
-
-            if(is_root_thread)
-            {
-                map[map_i] = map_i;
-                map[i] = map_ii;
-            };
-
-            __syncthreads();
-
-            // ---------------------------------------
-            // swap columns C( 0:(n-1),map_i) and C( 0:(n-1),map_ii)
-            // ---------------------------------------
-
-            for(auto k = k_start; k < n; k += k_inc)
-            {
-                auto const k_map_i = k + (map_i * static_cast<int64_t>(ldc));
-                auto const k_map_ii = k + (map_ii * static_cast<int64_t>(ldc));
-
-                auto const ctemp = C[k_map_i];
-                C[k_map_i] = C[k_map_ii];
-                C[k_map_ii] = ctemp;
-            };
-
-            __syncthreads();
-        };
-    };
 }
 
 template <typename S, typename I>
@@ -1128,133 +1139,6 @@ __device__ static void selection_sort_descending(const I n, S* D, I* map = nullp
 }
 
 template <typename S, typename I>
-__device__ static void shell_sort_descending(const I n, S* a, I* map = nullptr)
-{
-    // -----------------------------------------------
-    // Sort array a[0...(n-1)] and generate permutation vector
-    // in map[] if map[] is available
-    // Note: performs in a single thread block
-    // -----------------------------------------------
-
-    if((n <= 0) || (a == nullptr))
-    {
-        return;
-    };
-
-    // ------------------------
-    // Shell sort (from wikipedia)
-    // Sort an array a[0...(n-1)].
-    // ------------------------
-
-    bool const is_root_thread
-        = (hipThreadIdx_x == 0) && (hipThreadIdx_y == 0) && (hipThreadIdx_z == 0);
-
-    int constexpr ngaps = 8;
-    int const gaps[ngaps] = {701, 301, 132, 57, 23, 10, 4, 1}; // # Ciura gap sequence
-
-    auto const k_start = hipThreadIdx_x + hipThreadIdx_y * hipBlockDim_x
-        + hipThreadIdx_z * (hipBlockDim_x * hipBlockDim_y);
-
-    auto const k_inc = (hipBlockDim_x * hipBlockDim_y) * hipBlockDim_z;
-
-    bool const has_map = (map != nullptr);
-
-    __syncthreads();
-    if(has_map)
-    {
-        for(auto k = k_start; k < n; k += k_inc)
-        {
-            map[k] = k;
-        };
-    };
-    __syncthreads();
-
-    // ---------------------------------------------------------------
-    // perform sorting to generate permutation vector using shell sort
-    // ---------------------------------------------------------------
-    if(is_root_thread)
-    {
-        // --------------------------------------------------------------------------
-        // Start with the largest gap and work down to a gap of 1
-        // similar to insertion sort but instead of 1, gap is being used in each step
-        // --------------------------------------------------------------------------
-        for(auto igap = 0; igap < ngaps; igap++)
-        {
-            auto const gap = gaps[igap];
-
-            // -----------------------------------------------------
-            // Do a gapped insertion sort for every elements in gaps
-            // Each loop leaves a[0..gap-1] in gapped order
-            // -----------------------------------------------------
-            for(rocblas_int i = gap; i < n; i += 1)
-            {
-                {
-                    // -----------------------------------------------
-                    // save a[i] in temp and make a hole at position i
-                    // -----------------------------------------------
-                    S const temp = a[i];
-                    auto const itemp = (has_map) ? map[i] : 0;
-
-                    // ---------------------------------------------------------------------------------
-                    // shift earlier gap-sorted elements up until the correct location for a[i] is found
-                    // ---------------------------------------------------------------------------------
-                    auto j = i;
-
-                    while((j >= gap) && (a[j - gap] < temp))
-                    {
-                        a[j] = a[j - gap];
-                        if(has_map)
-                        {
-                            map[j] = map[j - gap];
-                        };
-                        j -= gap;
-                    };
-
-                    // ----------------------------------------------------
-                    // put temp (the original a[i]) in its correct location
-                    // ----------------------------------------------------
-                    a[j] = temp;
-                    if(has_map)
-                    {
-                        map[j] = itemp;
-                    };
-                };
-            };
-        };
-    };
-    __syncthreads();
-#ifdef NDEBUG
-#else
-    if(n >= 2)
-    {
-        for(auto k = k_start; k < (n - 1); k += k_inc)
-        {
-            assert(a[k] >= a[k + 1]);
-        };
-    };
-    __syncthreads();
-#endif
-}
-
-template <typename S, typename I>
-__device__ static void shell_sort(const I n, S* a, I* map = nullptr, const bool is_ascending = true)
-{
-    if((n <= 0) || (a == nullptr))
-    {
-        return;
-    };
-
-    if(is_ascending)
-    {
-        shell_sort_ascending(n, a, map);
-    }
-    else
-    {
-        shell_sort_descending(n, a, map);
-    };
-}
-
-template <typename S, typename I>
 __device__ static void selection_sort(const I n, S* a, I* map = nullptr, const bool is_ascending = true)
 {
     if((n <= 0) || (a == nullptr))
@@ -1270,4 +1154,82 @@ __device__ static void selection_sort(const I n, S* a, I* map = nullptr, const b
     {
         selection_sort_descending(n, a, map);
     };
+}
+
+template <typename T, typename I>
+__device__ static void permute_swap(const I n, T* C, I ldc, I* map)
+{
+    // --------------------------------------------
+    // perform swaps to implement permutation vector
+    // the permutation vector will be restored to the
+    // identity permutation 0,1,2,...
+    //
+    // Note: performs in a thread block
+    // --------------------------------------------
+    {
+        bool const has_work = (n >= 1) && (C != nullptr) && (ldc >= 1) && (map != nullptr);
+        if(!has_work)
+        {
+            return;
+        };
+    };
+
+    auto const tid = hipThreadIdx_x + hipThreadIdx_y * hipBlockDim_x
+        + hipThreadIdx_z * (hipBlockDim_x * hipBlockDim_y);
+
+    auto const nthreads = (hipBlockDim_x * hipBlockDim_y) * hipBlockDim_z;
+
+    auto const k_start = tid;
+    auto const k_inc = nthreads;
+
+    bool const is_root_thread = (tid == 0);
+
+    for(I i = 0; i < n; i++)
+    {
+        __syncthreads();
+
+        while(map[i] != i)
+        {
+            auto const map_i = map[i];
+            auto const map_ii = map[map[i]];
+
+            __syncthreads();
+
+            if(is_root_thread)
+            {
+                map[map_i] = map_i;
+                map[i] = map_ii;
+            };
+
+            __syncthreads();
+
+            // ---------------------------------------
+            // swap columns C( 0:(n-1),map_i) and C( 0:(n-1),map_ii)
+            // ---------------------------------------
+
+            for(auto k = k_start; k < n; k += k_inc)
+            {
+                auto const k_map_i = k + (map_i * static_cast<int64_t>(ldc));
+                auto const k_map_ii = k + (map_ii * static_cast<int64_t>(ldc));
+
+                auto const ctemp = C[k_map_i];
+                C[k_map_i] = C[k_map_ii];
+                C[k_map_ii] = ctemp;
+            };
+
+            __syncthreads();
+        };
+    };
+#ifdef NDEBUG
+#else
+    // ----------------------------------------------------------
+    // extra check that map[] is restored to identity permutation
+    // ----------------------------------------------------------
+    __syncthreads();
+    for(auto k = k_start; k < n; k += k_inc)
+    {
+        assert(map[k] == k);
+    };
+    __syncthreads();
+#endif
 }
