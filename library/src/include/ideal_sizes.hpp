e7dfacc138170e11821581aa614369b1884e3512
Eduardo D'Azevedo <Eduardo.DAzevedo@amd.com>
2024-04-30 16:10:29 -0400

    set stopping nb for getrf_nopvit

--- library/src/include/ideal_sizes.hpp
+++ library/src/include/ideal_sizes.hpp
@@ -399,8 +399,7 @@
 // terminate recursion if matrix can fit in cache
 // --------------------------------------------
 #ifndef GETRF_NOPIV_STOPPING_NB
-// #define GETRF_NOPIV_STOPPING_NB(T) ((sizeof(T) == 4) ? 1408 : (sizeof(T) == 8) ? 1024 : 704)
-#define GETRF_NOPIV_STOPPING_NB(T) GETRF_NOPIV_BLOCKSIZE(T)
+#define GETRF_NOPIV_STOPPING_NB(T) ((sizeof(T) == 4) ? 1408 : (sizeof(T) == 8) ? 1024 : 704)
 #endif
 
 /****************************** getri *****************************************
