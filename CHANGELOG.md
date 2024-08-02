a791b55de6da2a670a3889f5e288b4cafdb77630
Troy Alderson <58866654+tfalders@users.noreply.github.com>
2024-08-02 17:22:17 -0600

    Workaround: disable small-size kernels for wave 32 architectures

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -12,7 +12,7 @@ Full documentation for rocSOLVER is available at the [rocSOLVER documentation](h
     - GEQRF_64 (with batched and strided\_batched versions)
 
 ### Optimized
-- Improved performanced of LARFG, LARF, and downstream functions such as GEQR2 and GEQRF
+- Improved performanced of LARFG, LARF, and downstream functions such as GEQR2 and GEQRF on wave64 architectures
 
 ### Changed
 - The rocsparse library is now an optional dependency at runtime. If rocsparse
--- library/src/auxiliary/rocauxiliary_larfg.hpp
+++ library/src/auxiliary/rocauxiliary_larfg.hpp
@@ -210,8 +210,16 @@ rocblas_status rocsolver_larfg_template(rocblas_handle handle,
     // if n is small, use small-size kernel
     if(n <= LARFG_SSKER_MAX_N)
     {
-        return larfg_run_small(handle, n, alpha, shifta, stridex, x, shiftx, incx, stridex, tau,
-                               strideP, batch_count);
+        // TODO: Some architectures have failures in sygvx with small-size kernels enabled, more investigation needed
+        int device;
+        HIP_CHECK(hipGetDevice(&device));
+        hipDeviceProp_t deviceProperties;
+        HIP_CHECK(hipGetDeviceProperties(&deviceProperties, device));
+        if(deviceProperties.warpSize >= 64)
+        {
+            return larfg_run_small(handle, n, alpha, shifta, stridex, x, shiftx, incx, stridex, tau,
+                                   strideP, batch_count);
+        }
     }
 
     // everything must be executed with scalars on the device
