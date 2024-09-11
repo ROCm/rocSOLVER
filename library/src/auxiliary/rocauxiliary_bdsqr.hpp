784b6630e8022adb061f366f8bdd703e52f03397
Troy Alderson <58866654+tfalders@users.noreply.github.com>
2024-09-11 11:45:49 -0600

    Addressed review comments

--- library/src/auxiliary/rocauxiliary_bdsqr.hpp
+++ library/src/auxiliary/rocauxiliary_bdsqr.hpp
@@ -64,7 +64,10 @@ __device__ T bdsqr_estimate(const rocblas_int n, T* D, T* E, int t2b, T tol, int
             smin = -1;
             break;
         }
+
+        // Note: Order of operations is important to prevent t from being rounded down to zero
         t = std::abs(D[jd]) / (t + std::abs(E[je])) * t;
+
         smin = (t < smin) ? t : smin;
     }
 
