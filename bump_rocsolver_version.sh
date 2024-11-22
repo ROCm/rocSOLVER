6a1850f459f2ae51c4a16da961a89af3908fcf9c
jzuniga-amd <juan.zuniga-anaya@amd.com>
2024-11-21 17:11:48 -0700

    bump versions and prepare changelog

--- bump_rocsolver_version.sh
+++ bump_rocsolver_version.sh
@@ -4,11 +4,11 @@
 # Edit script to bump versions for new development cycle/release.
 
 # for rocSOLVER version string
-OLD_ROCSOLVER_VERSION="3\.28\.0"
-NEW_ROCSOLVER_VERSION="3.29.0"
+OLD_ROCSOLVER_VERSION="3\.29\.0"
+NEW_ROCSOLVER_VERSION="3.30.0"
 sed -i "s/${OLD_ROCSOLVER_VERSION}/${NEW_ROCSOLVER_VERSION}/g" CMakeLists.txt
 
 # for rocSOLVER library name
-OLD_ROCSOLVER_SOVERSION="0\.4"
-NEW_ROCSOLVER_SOVERSION="0.5"
+OLD_ROCSOLVER_SOVERSION="0\.5"
+NEW_ROCSOLVER_SOVERSION="0.6"
 sed -i "s/${OLD_ROCSOLVER_SOVERSION}/${NEW_ROCSOLVER_SOVERSION}/g" library/CMakeLists.txt
