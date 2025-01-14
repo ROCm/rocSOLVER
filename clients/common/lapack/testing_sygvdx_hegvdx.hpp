a55dcd8e5afa9186e767cda7bb63806f09a66b1c
Julio Machado Silva <161654951+jmachado-amd@users.noreply.github.com>
2025-01-14 10:43:30 -0700

    Fix tolerance for HEGVDX tests (#879)
    As part of PR #810, the tolerances for `sygvdx` and `hegvdx` tests were
    set too tight, which made 3 of `hegvdx` daily lapack tests fail.
    
    This PR changes the tolerance to allow all tests to pass on all architectures.
    
    [ROCm/rocSOLVER commit: adeb5346ae67d8e13fedd0917f7ad987e5eedb57]

--- clients/common/lapack/testing_sygvdx_hegvdx.hpp
+++ clients/common/lapack/testing_sygvdx_hegvdx.hpp
@@ -1108,9 +1108,9 @@ void testing_sygvdx_hegvdx(Arguments& argus)
     }
 
     // validate results for rocsolver-test
-    // using 4 * n * machine_precision as tolerance
+    // using 5 * n * machine_precision as tolerance
     if(argus.unit_check)
-        ROCSOLVER_TEST_CHECK(T, max_error, 4 * n);
+        ROCSOLVER_TEST_CHECK(T, max_error, 5 * n);
 
     // output results for rocsolver-bench
     if(argus.timing)
