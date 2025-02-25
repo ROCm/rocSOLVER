37116d8e8e980d6ebcb101b1aaf5c29680600a10
amd-garydeng <garydeng@amd.com>
2025-02-25 11:04:54 -0700

    turn on for precheckin

--- .jenkins/precheckin.groovy
+++ .jenkins/precheckin.groovy
@@ -39,7 +39,7 @@ def runCI =
         platform, project->
 
         def gfilter = 'checkin*'
-        commonGroovy.runTestCommand(platform, project, gfilter)
+        commonGroovy.runTestCommand(platform, project, gfilter, true)
     }
 
     def packageCommand =
