c4abb22b23d66545b8365f021a440dfc2752cf84
Troy Alderson <58866654+tfalders@users.noreply.github.com>
2025-03-07 14:13:30 -0700

    Use stedc instead of sterf in syevd (#901)
    * Test hybrid syevd
    
    * Use stedc in no vector case
    
    * Use sterf for hybrid only
    
    * Addressed review comment
    
    * Addressed review comments

--- clients/common/lapack/testing_syev_heev.hpp
+++ clients/common/lapack/testing_syev_heev.hpp
@@ -1,5 +1,5 @@
 /* **************************************************************************
- * Copyright (C) 2021-2024 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (C) 2021-2025 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -398,7 +398,7 @@ void testing_syev_heev(Arguments& argus)
     rocblas_int bc = argus.batch_count;
     rocblas_int hot_calls = argus.iters;
 
-    if(argus.alg_mode)
+    if(argus.alg_mode == 1)
     {
         EXPECT_ROCBLAS_STATUS(
             rocsolver_set_alg_mode(handle, rocsolver_function_sterf, rocsolver_alg_mode_hybrid),
