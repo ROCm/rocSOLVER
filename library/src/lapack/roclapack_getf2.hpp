aa45c144aa3a69723ee3dbb38663cbb6f3eebec2
Eduardo D'Azevedo <Eduardo.DAzevedo@amd.com>
2025-04-01 00:11:14 -0400

    use 64bit lda  for index calculations

--- library/src/lapack/roclapack_getf2.hpp
+++ library/src/lapack/roclapack_getf2.hpp
@@ -4,7 +4,7 @@
  *     Univ. of Tennessee, Univ. of California Berkeley,
  *     Univ. of Colorado Denver and NAG Ltd..
  *     December 2016
- * Copyright (C) 2019-2024 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (C) 2019-2025 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -66,8 +66,8 @@ ROCSOLVER_KERNEL void getf2_check_singularity(const I n,
                                               const I j,
                                               U AA,
                                               const rocblas_stride shiftA,
-                                              const I inca,
-                                              const I lda,
+                                              const I inca_arg,
+                                              const I lda_arg,
                                               const rocblas_stride strideA,
                                               I* ipivA,
                                               const rocblas_stride shiftP,
@@ -80,6 +80,8 @@ ROCSOLVER_KERNEL void getf2_check_singularity(const I n,
                                               const rocblas_stride stridePI)
 {
     using S = decltype(std::real(T{}));
+    rocblas_stride const lda = lda_arg;
+    rocblas_stride const inca = inca_arg;
 
     const I id = hipBlockIdx_y;
     I tid = hipBlockIdx_x * static_cast<I>(hipBlockDim_x) + hipThreadIdx_x;
@@ -127,14 +129,16 @@ template <typename T, typename I, typename INFO, typename U>
 ROCSOLVER_KERNEL void getf2_npvt_check_singularity(const I j,
                                                    U AA,
                                                    const rocblas_stride shiftA,
-                                                   const I inca,
-                                                   const I lda,
+                                                   const I inca_arg,
+                                                   const I lda_arg,
                                                    const rocblas_stride strideA,
                                                    T* pivot_val,
                                                    INFO* info,
                                                    const I offset)
 {
     using S = decltype(std::real(T{}));
+    rocblas_stride const lda = lda_arg;
+    rocblas_stride const inca = inca_arg;
 
     const I id = hipBlockIdx_y;
 
--- library/src/lapack/roclapack_getrf.hpp
+++ library/src/lapack/roclapack_getrf.hpp
@@ -4,7 +4,7 @@
  *     Univ. of Tennessee, Univ. of California Berkeley,
  *     Univ. of Colorado Denver and NAG Ltd..
  *     December 2016
- * Copyright (C) 2019-2024 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (C) 2019-2025 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -199,12 +199,15 @@ ROCSOLVER_KERNEL void getrf_row_permutate(const I n,
                                           const I blk,
                                           U AA,
                                           const rocblas_stride shiftA,
-                                          const I inca,
-                                          const I lda,
+                                          const I inca_arg,
+                                          const I lda_arg,
                                           const rocblas_stride strideA,
                                           I* pividx,
                                           const rocblas_stride stridePI)
 {
+    rocblas_stride const lda = lda_arg;
+    rocblas_stride const inca = inca_arg;
+
     I id = hipBlockIdx_z;
     I tx = hipThreadIdx_x;
     I ty = hipThreadIdx_y;
