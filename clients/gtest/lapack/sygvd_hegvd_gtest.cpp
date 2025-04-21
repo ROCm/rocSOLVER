144f6a31a43f669985a9176969f8bcb60c82158a
AGonzales-amd <angonzal@amd.com>
2025-04-21 09:27:17 -0600

    remove sygvd test

--- clients/gtest/lapack/sygvd_hegvd_gtest.cpp
+++ clients/gtest/lapack/sygvd_hegvd_gtest.cpp
@@ -116,92 +116,92 @@ protected:
     }
 };
 
-class SYGVD : public SYGVD_HEGVD
-{
-};
+// class SYGVD : public SYGVD_HEGVD
+// {
+// };
 
-class HEGVD : public SYGVD_HEGVD
-{
-};
+// class HEGVD : public SYGVD_HEGVD
+// {
+// };
 
-// non-batch tests
+// // non-batch tests
 
-TEST_P(SYGVD, __float)
-{
-    run_tests<false, false, float>();
-}
+// TEST_P(SYGVD, __float)
+// {
+//     run_tests<false, false, float>();
+// }
 
-TEST_P(SYGVD, __double)
-{
-    run_tests<false, false, double>();
-}
+// TEST_P(SYGVD, __double)
+// {
+//     run_tests<false, false, double>();
+// }
 
-TEST_P(HEGVD, __float_complex)
-{
-    run_tests<false, false, rocblas_float_complex>();
-}
+// TEST_P(HEGVD, __float_complex)
+// {
+//     run_tests<false, false, rocblas_float_complex>();
+// }
 
-TEST_P(HEGVD, __double_complex)
-{
-    run_tests<false, false, rocblas_double_complex>();
-}
+// TEST_P(HEGVD, __double_complex)
+// {
+//     run_tests<false, false, rocblas_double_complex>();
+// }
 
-// batched tests
+// // batched tests
 
-TEST_P(SYGVD, batched__float)
-{
-    run_tests<true, true, float>();
-}
+// TEST_P(SYGVD, batched__float)
+// {
+//     run_tests<true, true, float>();
+// }
 
-TEST_P(SYGVD, batched__double)
-{
-    run_tests<true, true, double>();
-}
+// TEST_P(SYGVD, batched__double)
+// {
+//     run_tests<true, true, double>();
+// }
 
-TEST_P(HEGVD, batched__float_complex)
-{
-    run_tests<true, true, rocblas_float_complex>();
-}
+// TEST_P(HEGVD, batched__float_complex)
+// {
+//     run_tests<true, true, rocblas_float_complex>();
+// }
 
-TEST_P(HEGVD, batched__double_complex)
-{
-    run_tests<true, true, rocblas_double_complex>();
-}
+// TEST_P(HEGVD, batched__double_complex)
+// {
+//     run_tests<true, true, rocblas_double_complex>();
+// }
 
-// strided_batched cases
+// // strided_batched cases
 
-TEST_P(SYGVD, strided_batched__float)
-{
-    run_tests<false, true, float>();
-}
+// TEST_P(SYGVD, strided_batched__float)
+// {
+//     run_tests<false, true, float>();
+// }
 
-TEST_P(SYGVD, strided_batched__double)
-{
-    run_tests<false, true, double>();
-}
+// TEST_P(SYGVD, strided_batched__double)
+// {
+//     run_tests<false, true, double>();
+// }
 
-TEST_P(HEGVD, strided_batched__float_complex)
-{
-    run_tests<false, true, rocblas_float_complex>();
-}
+// TEST_P(HEGVD, strided_batched__float_complex)
+// {
+//     run_tests<false, true, rocblas_float_complex>();
+// }
 
-TEST_P(HEGVD, strided_batched__double_complex)
-{
-    run_tests<false, true, rocblas_double_complex>();
-}
+// TEST_P(HEGVD, strided_batched__double_complex)
+// {
+//     run_tests<false, true, rocblas_double_complex>();
+// }
 
-INSTANTIATE_TEST_SUITE_P(daily_lapack,
-                         SYGVD,
-                         Combine(ValuesIn(large_matrix_size_range), ValuesIn(type_range)));
+// INSTANTIATE_TEST_SUITE_P(daily_lapack,
+//                          SYGVD,
+//                          Combine(ValuesIn(large_matrix_size_range), ValuesIn(type_range)));
 
-INSTANTIATE_TEST_SUITE_P(checkin_lapack,
-                         SYGVD,
-                         Combine(ValuesIn(matrix_size_range), ValuesIn(type_range)));
+// INSTANTIATE_TEST_SUITE_P(checkin_lapack,
+//                          SYGVD,
+//                          Combine(ValuesIn(matrix_size_range), ValuesIn(type_range)));
 
-INSTANTIATE_TEST_SUITE_P(daily_lapack,
-                         HEGVD,
-                         Combine(ValuesIn(large_matrix_size_range), ValuesIn(type_range)));
+// INSTANTIATE_TEST_SUITE_P(daily_lapack,
+//                          HEGVD,
+//                          Combine(ValuesIn(large_matrix_size_range), ValuesIn(type_range)));
 
-INSTANTIATE_TEST_SUITE_P(checkin_lapack,
-                         HEGVD,
-                         Combine(ValuesIn(matrix_size_range), ValuesIn(type_range)));
+// INSTANTIATE_TEST_SUITE_P(checkin_lapack,
+//                          HEGVD,
+//                          Combine(ValuesIn(matrix_size_range), ValuesIn(type_range)));
