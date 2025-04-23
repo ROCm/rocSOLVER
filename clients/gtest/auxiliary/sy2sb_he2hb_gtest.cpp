ef20eb7de876216d77f1d784baa407ec6d8add10
AGonzales-amd <angonzal@amd.com>
2025-04-23 17:27:01 -0600

    sy2sb lower

--- clients/gtest/auxiliary/sy2sb_he2hb_gtest.cpp
+++ clients/gtest/auxiliary/sy2sb_he2hb_gtest.cpp
@@ -32,18 +32,18 @@
  using ::testing::Values;
  using ::testing::ValuesIn;
  using namespace std;
- 
+
  typedef std::tuple<vector<int>, int> sy2sb_he2hb_tuple;
- 
+
  // each matrix_size_range is a {n, lda, kd, ldab}
- 
+
  // each op_range is a ul
  // if ul = 0, then uplo = 'L'
  // if ul = 1, then uplo = 'U'
- 
+
  // case when n = 0 and k = 0 will also execute the bad arguments test
  // (null handle, null pointers and invalid values)
- 
+
  // for checkin_lapack tests
  const vector<vector<int>> matrix_size_range = {
      // quick return
@@ -57,32 +57,32 @@
      {10, 15, 3, 5},
      {128, 128, 12, 13},
     };
- 
+
  const vector<int> op_range = {1, 0};
- 
+
  // for daily_lapack tests
  const vector<vector<int>> large_matrix_size_range
      = {{152, 152, 64, 65}, {640, 640, 500, 501}, {1000, 1024, 800, 810}};
- 
+
  Arguments sy2sb_he2hb_setup_arguments(sy2sb_he2hb_tuple tup)
  {
      vector<int> matrix_size = std::get<0>(tup);
      int op = std::get<1>(tup);
- 
+
      Arguments arg;
- 
+
      arg.set<rocblas_int>("n", matrix_size[0]);
      arg.set<rocblas_int>("lda", matrix_size[1]);
      arg.set<rocblas_int>("k", matrix_size[2]);
      arg.set<rocblas_int>("ldb", matrix_size[3]);
 
      arg.set<char>("uplo", op ? 'U' : 'L');
- 
+
      arg.timing = 0;
- 
+
      return arg;
  }
- 
+
  class SY2SB_HE2HB : public ::TestWithParam<sy2sb_he2hb_tuple>
  {
  protected:
@@ -90,46 +90,45 @@
      {
          EXPECT_EQ(hipGetLastError(), hipSuccess);
      }
- 
+
      template <typename T>
      void run_tests()
      {
          Arguments arg = sy2sb_he2hb_setup_arguments(GetParam());
- 
+
         //  if(arg.peek<rocblas_int>("k") == 0 && arg.peek<rocblas_int>("n") == 0)
         //      testing_sy2sb_he2hb_bad_arg<T>();
- 
+
          testing_sy2sb_he2hb<T>(arg);
      }
  };
- 
+
  // non-batch tests
- 
+
  TEST_P(SY2SB_HE2HB, __float)
  {
      run_tests<float>();
  }
- 
+
  TEST_P(SY2SB_HE2HB, __double)
  {
      run_tests<double>();
  }
- 
+
  TEST_P(SY2SB_HE2HB, __float_complex)
  {
      run_tests<rocblas_float_complex>();
  }
- 
+
  TEST_P(SY2SB_HE2HB, __double_complex)
  {
      run_tests<rocblas_double_complex>();
  }
- 
+
  INSTANTIATE_TEST_SUITE_P(daily_lapack,
                           SY2SB_HE2HB,
                           Combine(ValuesIn(large_matrix_size_range), ValuesIn(op_range)));
- 
+
  INSTANTIATE_TEST_SUITE_P(checkin_lapack,
                           SY2SB_HE2HB,
                           Combine(ValuesIn(matrix_size_range), ValuesIn(op_range)));
- 
