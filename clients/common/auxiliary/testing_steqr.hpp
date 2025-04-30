0b91bd14c850102d1836f9621d0d090850b5637a
Troy Alderson <58866654+tfalders@users.noreply.github.com>
2025-04-29 18:06:11 -0600

    Added tests for hybrid steqr

--- clients/common/auxiliary/testing_steqr.hpp
+++ clients/common/auxiliary/testing_steqr.hpp
@@ -1,5 +1,5 @@
 /* **************************************************************************
- * Copyright (C) 2020-2024 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (C) 2020-2025 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -340,6 +340,19 @@ void testing_steqr(Arguments& argus)
 
     rocblas_evect evect = char2rocblas_evect(evectC);
     rocblas_int hot_calls = argus.iters;
+
+    if(argus.alg_mode == 1)
+    {
+        EXPECT_ROCBLAS_STATUS(
+            rocsolver_set_alg_mode(handle, rocsolver_function_steqr, rocsolver_alg_mode_hybrid),
+            rocblas_status_success);
+
+        rocsolver_alg_mode alg_mode;
+        EXPECT_ROCBLAS_STATUS(rocsolver_get_alg_mode(handle, rocsolver_function_steqr, &alg_mode),
+                              rocblas_status_success);
+
+        EXPECT_EQ(alg_mode, rocsolver_alg_mode_hybrid);
+    }
 
     // check non-supported values
     // N/A
