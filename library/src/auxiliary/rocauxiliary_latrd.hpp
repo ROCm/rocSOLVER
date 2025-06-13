eeb7e7fd47334eed4879c053c6c9a15e5e5a5420
Troy Alderson <58866654+tfalders@users.noreply.github.com>
2025-06-13 14:49:18 -0600

    Use cross-lane operations in new gemvt kernels

--- library/src/auxiliary/rocauxiliary_latrd.hpp
+++ library/src/auxiliary/rocauxiliary_latrd.hpp
@@ -1203,18 +1203,21 @@ ROCSOLVER_KERNEL void latrd_upper_computeW_gemvt_kernel(const rocblas_int mm,
         if(tx + n_full < n)
             res += conj(a[n_full]) * x[tx + n_full];
 
-        sdata[tx] = res;
+        // reduction of partial sums
+        res += shift_left(res, 1);
+        res += shift_left(res, 2);
+        res += shift_left(res, 4);
+        res += shift_left(res, 8);
+        res += shift_left(res, 16);
+        if(warpSize > 32)
+            res += shift_left(res, 32);
+        if(tx % warpSize == 0)
+            sdata[tx / warpSize] = res;
         __syncthreads();
-
-        // tree reduction of partial sums
-        for(int r = NB_X / 2; r > 0; r /= 2)
+        if(tx == 0)
         {
-            if(tx < r)
-            {
-                res += sdata[tx + r];
-                sdata[tx] = res;
-            }
-            __syncthreads();
+            for(rocblas_int k = 1; k < NB_X / warpSize; k++)
+                res += sdata[k];
         }
     }
 
@@ -1412,18 +1415,21 @@ ROCSOLVER_KERNEL void latrd_lower_computeW_gemvt_kernel(const rocblas_int mm,
         if(tx + n_full < n)
             res += conj(a[n_full]) * x[tx + n_full];
 
-        sdata[tx] = res;
+        // reduction of partial sums
+        res += shift_left(res, 1);
+        res += shift_left(res, 2);
+        res += shift_left(res, 4);
+        res += shift_left(res, 8);
+        res += shift_left(res, 16);
+        if(warpSize > 32)
+            res += shift_left(res, 32);
+        if(tx % warpSize == 0)
+            sdata[tx / warpSize] = res;
         __syncthreads();
-
-        // tree reduction of partial sums
-        for(int r = NB_X / 2; r > 0; r /= 2)
+        if(tx == 0)
         {
-            if(tx < r)
-            {
-                res += sdata[tx + r];
-                sdata[tx] = res;
-            }
-            __syncthreads();
+            for(rocblas_int k = 1; k < NB_X / warpSize; k++)
+                res += sdata[k];
         }
     }
 
