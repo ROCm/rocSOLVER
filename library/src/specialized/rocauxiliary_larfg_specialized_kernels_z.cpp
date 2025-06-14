b4a89263772d592268610d62e37aa90d62370bb8
Troy Alderson <58866654+tfalders@users.noreply.github.com>
2025-06-14 17:47:41 -0600

    Optimize latrd (#965)
    * use gemvt kernel for compute W
    
    * upper updateA gemvt
    
    * larfg kernel for latrd
    
    * Move shift_left device function to lib_device_helpers
    
    * dot_scale_axpy kernel for latrd
    
    * Use shared memory for dot_scale_axpy kernel
    
    * larfg beta is of type S
    
    * Replace larfg_kernel_small with latrd_larfg_kernel
    
    * Use cross-lane operations in new gemvt kernels
    
    * Cleanup
    
    ---------
    
    Co-authored-by: AGonzales-amd <angonzal@amd.com>

--- library/src/specialized/rocauxiliary_larfg_specialized_kernels_z.cpp
+++ library/src/specialized/rocauxiliary_larfg_specialized_kernels_z.cpp
@@ -1,5 +1,5 @@
 /* **************************************************************************
- * Copyright (C) 2024 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (C) 2024-2025 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,13 +32,13 @@ ROCSOLVER_BEGIN_NAMESPACE
 /*************************************************************
     Instantiate template methods using macros
 *************************************************************/
-INSTANTIATE_LARFG_SMALL(rocblas_double_complex, rocblas_int, rocblas_double_complex*);
-INSTANTIATE_LARFG_SMALL(rocblas_double_complex, rocblas_int, rocblas_double_complex* const*);
+INSTANTIATE_LARFG_SMALL(rocblas_double_complex, rocblas_int, double, rocblas_double_complex*);
+INSTANTIATE_LARFG_SMALL(rocblas_double_complex, rocblas_int, double, rocblas_double_complex* const*);
 
 #ifdef HAVE_ROCBLAS_64
 // 64-bit APIs
-INSTANTIATE_LARFG_SMALL(rocblas_double_complex, int64_t, rocblas_double_complex*);
-INSTANTIATE_LARFG_SMALL(rocblas_double_complex, int64_t, rocblas_double_complex* const*);
+INSTANTIATE_LARFG_SMALL(rocblas_double_complex, int64_t, double, rocblas_double_complex*);
+INSTANTIATE_LARFG_SMALL(rocblas_double_complex, int64_t, double, rocblas_double_complex* const*);
 #endif /* HAVE_ROCBLAS_64 */
 
 ROCSOLVER_END_NAMESPACE
