e37ccf786c70c2bd97cfa3700f908c9ffe545be8
Troy Alderson <58866654+tfalders@users.noreply.github.com>
2025-06-30 16:54:03 -0600

    Apply clang format

--- library/src/auxiliary/rocauxiliary_stedcx.hpp
+++ library/src/auxiliary/rocauxiliary_stedcx.hpp
@@ -1,5 +1,5 @@
 /************************************************************************
- * Copyright (C) 2024 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (C) 2024-2025 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -36,10 +36,9 @@
 
 ROCSOLVER_BEGIN_NAMESPACE
 
-// TODO: using macro STEDCX_EXTERNAL_GEMM = false for now. We can enable the use of 
-// external gemm updates once the development is completed for stedc.  
+// TODO: using macro STEDCX_EXTERNAL_GEMM = false for now. We can enable the use of
+// external gemm updates once the development is completed for stedc.
 #define STEDCX_EXTERNAL_GEMM false
-
 
 /***************** Device auxiliary functions *****************************************/
 /**************************************************************************************/
@@ -583,11 +582,11 @@ rocblas_status rocsolver_stedcx_template(rocblas_handle handle,
                                 eps, ssfmin, ssfmax);
 
         // c. find merged eigen vectors
-        ROCSOLVER_LAUNCH_KERNEL((stedc_mergeVectors_kernel<rocsolver_stedc_mode_bisection, STEDCX_EXTERNAL_GEMM, S>),
-                                dim3(numgrps3, STEDC_NUM_SPLIT_BLKS, batch_count), dim3(STEDC_BDIM),
-                                lmemsize3, stream, k, n, D, strideD, E, strideE, tempvect, 0, ldt,
-                                strideT, tmpz, tempgemm, splits);
-                
+        ROCSOLVER_LAUNCH_KERNEL(
+            (stedc_mergeVectors_kernel<rocsolver_stedc_mode_bisection, STEDCX_EXTERNAL_GEMM, S>),
+            dim3(numgrps3, STEDC_NUM_SPLIT_BLKS, batch_count), dim3(STEDC_BDIM), lmemsize3, stream,
+            k, n, D, strideD, E, strideE, tempvect, 0, ldt, strideT, tmpz, tempgemm, splits);
+
         // d. update level
         ROCSOLVER_LAUNCH_KERNEL((stedc_mergeUpdate_kernel<rocsolver_stedc_mode_bisection, S>),
                                 dim3(numgrps3, STEDC_NUM_SPLIT_BLKS, batch_count), dim3(STEDC_BDIM),
